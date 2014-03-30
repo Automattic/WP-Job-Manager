da707838171a79eb5fc1fc68718fe26768c4ae41
Mike Jolley <mike.jolley@me.com>
2014-03-30 11:55:18 +0100

    Move output logic to hooks for email + url methods. this allows other plugins to add content too.

--- templates/job-application.php
+++ templates/job-application.php
@@ -6,28 +6,10 @@
 
 		<div class="application_details">
 			<?php
-				switch ( $apply->type ) {
-					case 'email' :
-
-						echo '<p>' . sprintf( __( 'To apply for this job <strong>email your details to</strong> <a class="job_application_email" href="mailto:%1$s%2$s">%1$s</a>', 'wp-job-manager' ), $apply->email, '?subject=' . rawurlencode( $apply->subject ) ) . '</p>';
-
-						echo '<p>' . __( 'Apply using webmail: ', 'wp-job-manager' );
-
-						echo '<a href="' . 'https://mail.google.com/mail/?view=cm&fs=1&to=' . $apply->email . '&su=' . urlencode( $apply->subject ) .'" target="_blank" class="job_application_email">Gmail</a> / ';
-
-						echo '<a href="' . 'http://webmail.aol.com/Mail/ComposeMessage.aspx?to=' . $apply->email . '&subject=' . urlencode( $apply->subject ) .'" target="_blank" class="job_application_email">AOL</a> / ';
-
-						echo '<a href="' . 'http://compose.mail.yahoo.com/?to=' . $apply->email . '&subject=' . urlencode( $apply->subject ) .'" target="_blank" class="job_application_email">Yahoo</a> / ';
-
-						echo '<a href="' . 'http://mail.live.com/mail/EditMessageLight.aspx?n=&to=' . $apply->email . '&subject=' . urlencode( $apply->subject ) .'" target="_blank" class="job_application_email">Outlook</a>';
-
-						echo '</p>';
-
-					break;
-					case 'url' :
-						echo '<p>' . sprintf( __( 'To apply for this job please visit the following URL: <a href="%1$s">%1$s &rarr;</a>', 'wp-job-manager' ), $apply->url ) . '</p>';
-					break;
-				}
+				/**
+				 * job_manager_application_details_email or job_manager_application_details_url hook
+				 */
+				do_action( 'job_manager_application_details_' . $apply->type, $apply );
 			?>
 		</div>
 	</div>
