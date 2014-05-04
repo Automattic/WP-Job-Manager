d223ee210d3e429e35b76d1d98077b3c76106fb4
Mike Jolley <mike.jolley@me.com>
2014-05-04 15:01:35 +0100

    Update add ons page for wpjobmanager.com

--- includes/admin/class-wp-job-manager-addons.php
+++ includes/admin/class-wp-job-manager-addons.php
@@ -19,7 +19,7 @@ class WP_Job_Manager_Addons {
 
 		if ( false === ( $addons = get_transient( 'wp_job_manager_addons_html' ) ) ) {
 
-			$raw_addons = wp_remote_get( 'http://mikejolley.com/projects/wp-job-manager/add-ons/' );
+			$raw_addons = wp_remote_get( 'https://wpjobmanager.com/add-ons/' );
 
 			if ( ! is_wp_error( $raw_addons ) ) {
 
@@ -31,7 +31,7 @@ class WP_Job_Manager_Addons {
 				$dom->loadHTML( $raw_addons );
 
 				$xpath  = new DOMXPath( $dom );
-				$tags   = $xpath->query('//ul[@class="items"]');
+				$tags   = $xpath->query('//ul[@class="products"]');
 				foreach ( $tags as $tag ) {
 					$addons = $tag->ownerDocument->saveXML( $tag );
 					break;
@@ -39,15 +39,17 @@ class WP_Job_Manager_Addons {
 
 				$addons = wp_kses_post( $addons );
 
-				if ( $addons )
+				if ( $addons ) {
 					set_transient( 'wp_job_manager_addons_html', $addons, 60*60*24*7 ); // Cached for a week
+				}
 			}
 		}
 
 		?>
 		<div class="wrap wp_job_manager wp_job_manager_addons_wrap">
-			<div class="icon32 icon32-posts-job_listing" id="icon-edit"><br /></div>
 			<h2><?php _e( 'WP Job Manager Add-ons', 'wp-job-manager' ); ?></h2>
+			<div id="notice" class="updated below-h2"><p><?php printf( __( 'Buying multiple add-ons? <a href="%s">Check out the core add-on bundle &rarr;</a>', 'wp-job-manager' ), 'https://wpjobmanager.com/add-ons/bundle/' ); ?></p></div>
+			<p></p>
 			<?php echo $addons; ?>
 		</div>
 		<?php
