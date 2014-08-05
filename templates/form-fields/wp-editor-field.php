9d8b69ae6ec450d5031836cbea0bbeceac8b86c4
Mike Jolley <mike.jolley@me.com>
2014-08-05 21:23:51 +0100

    Fix editor field

--- templates/form-fields/wp-editor-field.php
+++ templates/form-fields/wp-editor-field.php
@@ -5,17 +5,17 @@ $editor = apply_filters( 'submit_job_form_wp_editor_args', array(
 	'textarea_rows' => 8,
 	'quicktags'     => false,
 	'tinymce'       => array(
-		'plugins'                       => 'paste',
+		'plugins'                       => 'paste,wplink',
 		'paste_as_text'                 => true,
 		'paste_auto_cleanup_on_paste'   => true,
 		'paste_remove_spans'            => true,
 		'paste_remove_styles'           => true,
 		'paste_remove_styles_if_webkit' => true,
 		'paste_strip_class_attributes'  => true,
-		'theme_advanced_buttons1'       => 'bold,italic,|,bullist,numlist,|,undo,redo,|,|,code',
-		'theme_advanced_buttons2'       => '',
-		'theme_advanced_buttons3'       => '',
-		'theme_advanced_buttons4'       => ''
+		'toolbar1'                      => 'bold,italic,|,bullist,numlist,|,link,unlink,|,undo,redo,|,|,code',
+		'toolbar2'                      => '',
+		'toolbar3'                      => '',
+		'toolbar4'                      => ''
 	),
 ) );
 wp_editor( isset( $field['value'] ) ? esc_textarea( $field['value'] ) : '', $key, $editor );
