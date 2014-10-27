a1fea83d7734d4661984450c7dc8b3d9760c9cdd
Mike Jolley <mike.jolley@me.com>
2014-10-27 18:14:32 +0000

    Filters for content job listing meta.

--- templates/content-job_listing.php
+++ templates/content-job_listing.php
@@ -12,8 +12,12 @@
 			<?php the_job_location( false ); ?>
 		</div>
 		<ul class="meta">
+			<?php do_action( 'job_listing_meta_start' ); ?>
+
 			<li class="job-type <?php echo get_the_job_type() ? sanitize_title( get_the_job_type()->slug ) : ''; ?>"><?php the_job_type(); ?></li>
 			<li class="date"><date><?php printf( __( '%s ago', 'wp-job-manager' ), human_time_diff( get_post_time( 'U' ), current_time( 'timestamp' ) ) ); ?></date></li>
+
+			<?php do_action( 'job_listing_meta_end' ); ?>
 		</ul>
 	</a>
 </li>
