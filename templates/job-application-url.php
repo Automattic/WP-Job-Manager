bbc89e0cb7edb83bd0cff8fe8d003abdb3a9e2cb
Mike Jolley <mike.jolley@me.com>
2015-06-13 11:53:22 +0100

    nofollow apply links Closes #478

--- templates/job-application-url.php
+++ templates/job-application-url.php
@@ -1,1 +1,1 @@
-<p><?php printf( __( 'To apply for this job please visit the following URL: <a href="%1$s" target="_blank">%1$s &rarr;</a>', 'wp-job-manager' ), $apply->url ); ?></p>
+<p><?php _e( 'To apply for this job please visit the following URL:', 'wp-job-manager' ); ?> <a href="<?php echo esc_url( $apply->url ); ?>" target="_blank" rel="nofollow"><?php echo esc_html( $apply->url ); ?> &rarr;</a></p>
