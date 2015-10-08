0ce50dab121505a55feb1df4475735612c4dc9d8
Mike Jolley <mike.jolley@me.com>
2015-10-08 15:02:52 +0100

    Add class names to buttons
    Closes #541

--- includes/forms/class-wp-job-manager-form-submit-job.php
+++ includes/forms/class-wp-job-manager-form-submit-job.php
@@ -615,8 +615,8 @@ class WP_Job_Manager_Form_Submit_Job extends WP_Job_Manager_Form {
 			?>
 			<form method="post" id="job_preview" action="<?php echo esc_url( $action ); ?>">
 				<div class="job_listing_preview_title">
-					<input type="submit" name="continue" id="job_preview_submit_button" class="button" value="<?php echo apply_filters( 'submit_job_step_preview_submit_text', __( 'Submit Listing', 'wp-job-manager' ) ); ?>" />
-					<input type="submit" name="edit_job" class="button" value="<?php _e( 'Edit listing', 'wp-job-manager' ); ?>" />
+					<input type="submit" name="continue" id="job_preview_submit_button" class="button job-manager-button-submit-listing" value="<?php echo apply_filters( 'submit_job_step_preview_submit_text', __( 'Submit Listing', 'wp-job-manager' ) ); ?>" />
+					<input type="submit" name="edit_job" class="button job-manager-button-edit-listing" value="<?php _e( 'Edit listing', 'wp-job-manager' ); ?>" />
 					<input type="hidden" name="job_id" value="<?php echo esc_attr( $this->job_id ); ?>" />
 					<input type="hidden" name="step" value="<?php echo esc_attr( $this->step ); ?>" />
 					<input type="hidden" name="job_manager_form" value="<?php echo $this->form_name; ?>" />
