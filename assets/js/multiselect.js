1cbdd3d699dbf9f92ddfdd8875dad8f64d02a860
Myles McNamara <myles@smyl.es>
2015-10-19 17:02:04 -0400

    fix js formatting

--- assets/js/multiselect.js
+++ assets/js/multiselect.js
@@ -1,3 +1,3 @@
-jQuery(function() {
-	jQuery('.job-manager-multiselect').chosen( job_manager_chosen_multiselect_args );
+jQuery(function(){
+	jQuery( '.job-manager-multiselect' ).chosen( job_manager_chosen_multiselect_args );
 });
