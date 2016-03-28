7099994b6a2097600ad9d9054ca3d711747263f3
George Stephanis <georgestephanis@automattic.com>
2016-03-28 12:52:27 -0400

    job-dashboard.js: use `window.confirm` so we don't need to declare `confirm` as a global.

--- assets/js/job-dashboard.js
+++ assets/js/job-dashboard.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function($) {
 
 	$('.job-dashboard-action-delete').click(function() {
-		return confirm( job_manager_job_dashboard.i18n_confirm_delete );
+		return window.confirm( job_manager_job_dashboard.i18n_confirm_delete );
 	});
 
 });
