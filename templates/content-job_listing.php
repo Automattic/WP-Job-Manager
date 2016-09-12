cda3c926b6ac8fc784196d796ff711ee34f905c9
Christian Nolen <cnolen@emagine.com>
2016-09-12 13:00:50 -0400

    Updated the date output for job listings to use the "the_job_publish_date" function.

--- templates/content-job_listing.php
+++ templates/content-job_listing.php
@@ -16,7 +16,7 @@
 			<?php do_action( 'job_listing_meta_start' ); ?>
 
 			<li class="job-type <?php echo get_the_job_type() ? sanitize_title( get_the_job_type()->slug ) : ''; ?>"><?php the_job_type(); ?></li>
-			<li class="date"><date><?php printf( __( '%s ago', 'wp-job-manager' ), human_time_diff( get_post_time( 'U' ), current_time( 'timestamp' ) ) ); ?></date></li>
+			<li class="date"><date><?php the_job_publish_date(); ?></date></li>
 
 			<?php do_action( 'job_listing_meta_end' ); ?>
 		</ul>
