7ce9797857765c3a04b28806ac9864c3df45561b
Christian Nolen <cnolen@emagine.com>
2016-09-12 12:58:53 -0400

    Added "get_the_job_publish_date" function that returns the date for a job using the display format selected in the plugin settings.

--- wp-job-manager-template.php
+++ wp-job-manager-template.php
@@ -283,6 +283,22 @@ function the_job_publish_date( $post = null ) {
 
 
 /**
+ * get_the_job_publish_date function.
+ * @param mixed $post (default: null)
+ * @return [type]
+ */
+function get_the_job_publish_date( $post = null ) {
+	$date_format = get_option( 'job_manager_date_format' );
+
+	if ( $date_format === 'default' ) {
+		return get_the_time( get_option( 'date_format' ) );
+	} else {
+		return sprintf( __( 'Posted %s ago', 'wp-job-manager' ), human_time_diff( get_post_time( 'U' ), current_time( 'timestamp' ) ) );
+	}
+}
+
+
+/**
  * the_job_location function.
  * @param  boolean $map_link whether or not to link to google maps
  * @return [type]
