3b9bc404dda228b33562be48e6ee37f69ac97b51
Spencer Finnell <spencerfinnell@gmail.com>
2017-03-06 11:23:00 -0500

    Allow AJAX searching to return JSON early without building template HTML. See #887
    - Also removes “showing” message building that is no longer used.
    - Uses proper brace styling for HTML generation.

--- includes/class-wp-job-manager-ajax.php
+++ includes/class-wp-job-manager-ajax.php
@@ -80,7 +80,9 @@ class WP_Job_Manager_Ajax {
 	public function get_listings() {
 		global $wp_post_types;
 
-		$result            = array();
+		$result            = array(
+			'found_jobs' => false
+		);
 		$search_location   = sanitize_text_field( stripslashes( $_REQUEST['search_location'] ) );
 		$search_keywords   = sanitize_text_field( stripslashes( $_REQUEST['search_keywords'] ) );
 		$search_categories = isset( $_REQUEST['search_categories'] ) ? $_REQUEST['search_categories'] : '';
@@ -115,71 +117,9 @@ class WP_Job_Manager_Ajax {
 			$args['orderby']  = 'featured' === $orderby ? 'date' : $orderby;
 		}
 
-		ob_start();
-
 		$jobs = get_job_listings( apply_filters( 'job_manager_get_listings_args', $args ) );
 
-		$result['found_jobs'] = false;
-
-		if ( $jobs->have_posts() ) : $result['found_jobs'] = true; ?>
-
-			<?php while ( $jobs->have_posts() ) : $jobs->the_post(); ?>
-
-				<?php get_job_manager_template_part( 'content', 'job_listing' ); ?>
-
-			<?php endwhile; ?>
-
-		<?php else : ?>
-
-			<?php get_job_manager_template_part( 'content', 'no-jobs-found' ); ?>
-
-		<?php endif;
-
-		$result['html']    = ob_get_clean();
 		$result['showing'] = array();
-
-		// Generate 'showing' text
-		$showing_types = array();
-		$unmatched     = false;
-
-		foreach ( $types as $type ) {
-			if ( is_array( $filter_job_types ) && in_array( $type->slug, $filter_job_types ) ) {
-				$showing_types[] = $type->name;
-			} else {
-				$unmatched = true;
-			}
-		}
-
-		if ( sizeof( $showing_types ) == 1 ) {
-			$result['showing'][] = implode( ', ', $showing_types );
-		} elseif ( $unmatched && $showing_types ) {
-			$last_type           = array_pop( $showing_types );
-			$result['showing'][] = implode( ', ', $showing_types ) . " &amp; $last_type";
-		}
-
-		if ( $search_categories ) {
-			$showing_categories = array();
-
-			foreach ( $search_categories as $category ) {
-				$category_object = get_term_by( is_numeric( $category ) ? 'id' : 'slug', $category, 'job_listing_category' );
-
-				if ( ! is_wp_error( $category_object ) ) {
-					$showing_categories[] = $category_object->name;
-				}
-			}
-
-			$result['showing'][] = implode( ', ', $showing_categories );
-		}
-
-		if ( $search_keywords ) {
-			$result['showing'][] = '&ldquo;' . $search_keywords . '&rdquo;';
-		}
-
-		$result['showing'][] = $post_type_label;
-
-		if ( $search_location ) {
-			$result['showing'][] = sprintf( __( 'located in &ldquo;%s&rdquo;', 'wp-job-manager' ), $search_location );
-		}
 
 		if ( 1 === sizeof( $result['showing'] ) ) {
 			$result['showing_all'] = true;
@@ -191,13 +131,8 @@ class WP_Job_Manager_Ajax {
 		} else {
 			$message = "";
 		}
-		
-		$search_values = array(
-				'location'   => $search_location,
-				'keywords'   => $search_keywords,
-				'categories' => $search_categories
-			);
-		$result['showing'] = apply_filters( 'job_manager_get_listings_custom_filter_text', $message, $search_values );
+
+		$result['showing'] = apply_filters( 'job_manager_get_listings_custom_filter_text', $message );
 
 		// Generate RSS link
 		$result['showing_links'] = job_manager_get_filtered_links( array(
@@ -213,6 +148,27 @@ class WP_Job_Manager_Ajax {
 		}
 
 		$result['max_num_pages'] = $jobs->max_num_pages;
+
+		// Give plugins the opportunity to control what JSON is returned
+		if ( $result == apply_filters( 'wp_job_manager_ajax_get_jobs', null, $result, $jobs ) ) {
+			return wp_send_json( apply_filters( 'job_manager_get_listings_result', $result, $jobs ) );
+		}
+
+		ob_start();
+
+		if ( $jobs->have_posts() ) {
+		   $result['found_jobs'] = true;
+
+		   while ( $jobs->have_posts() ) { 
+			   $jobs->the_post();
+
+				get_job_manager_template_part( 'content', 'job_listing' );
+			}
+		} else {
+			get_job_manager_template_part( 'content', 'no-jobs-found' );
+		}
+
+		$result['html'] = ob_get_clean();
 
 		wp_send_json( apply_filters( 'job_manager_get_listings_result', $result, $jobs ) );
 	}
