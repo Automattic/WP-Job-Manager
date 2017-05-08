d453692674b7836a9b8761411b7e07864e64f24a
Jake Oehler Morrison <jake.morrison@automattic.com>
2017-05-08 18:05:55 +0200

    Formatting fixes for WP_Job_Manager_API and replace `die()` with `wp_die()`
    Replace `die()` with `wp_die()` to allow for overriding when it
    actually dies (during tests).

--- includes/class-wp-job-manager-api.php
+++ includes/class-wp-job-manager-api.php
@@ -1,6 +1,8 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
 /**
  * Handles API requests for WP Job Manager.
@@ -36,8 +38,8 @@ class WP_Job_Manager_API {
 	 * Constructor.
 	 */
 	public function __construct() {
-		add_filter( 'query_vars', array( $this, 'add_query_vars'), 0 );
-		add_action( 'parse_request', array( $this, 'api_requests'), 0 );
+		add_filter( 'query_vars', array( $this, 'add_query_vars' ), 0 );
+		add_action( 'parse_request', array( $this, 'api_requests' ), 0 );
 	}
 
 	/**
@@ -64,8 +66,9 @@ class WP_Job_Manager_API {
 	public function api_requests() {
 		global $wp;
 
-		if ( ! empty( $_GET['job-manager-api'] ) )
+		if ( ! empty( $_GET['job-manager-api'] ) ) {
 			$wp->query_vars['job-manager-api'] = $_GET['job-manager-api'];
+		}
 
 		if ( ! empty( $wp->query_vars['job-manager-api'] ) ) {
 			// Buffer, we won't want any output here
@@ -75,15 +78,21 @@ class WP_Job_Manager_API {
 			$api = strtolower( esc_attr( $wp->query_vars['job-manager-api'] ) );
 
 			// Load class if exists
-			if ( has_action( 'job_manager_api_' . $api ) && class_exists( $api ) )
+			if ( has_action( 'job_manager_api_' . $api ) && class_exists( $api ) ) {
 				$api_class = new $api();
+			}
 
-			// Trigger actions
+			/**
+			 * Performs an API action.
+			 * The dynamic part of the action, $api, is the API action.
+			 *
+			 * @since 1.0.0
+			 */
 			do_action( 'job_manager_api_' . $api );
 
 			// Done, clear buffer and exit
 			ob_end_clean();
-			die('1');
+			wp_die();
 		}
 	}
 }
