b773f2da0f4e879f5af8051ae6f1793b74d175c2
Panos Kountanis <panosktn@gmail.com>
2017-05-23 18:10:49 +0300

    Add Tests for Settings Controller and Model refs #986

---
+++ tests/php/tests/includes/rest-api/test_class.wp-job-manager-models-settings.php
@@ -0,0 +1,43 @@
+<?php
+
+class WP_Test_WP_Job_Manager_Models_Settings extends WPJM_REST_TestCase {
+
+    function test_exists() {
+        $this->assertClassExists( 'WP_Job_Manager_Models_Settings' );
+    }
+
+    function test_validate_fail_when_invalid_page_id_settings() {
+        $setting_definition = $this->environment()
+            ->get()
+            ->model( WP_Job_Manager_Models_Settings::class );
+        $settings = $setting_definition->get_data_store()->get_entity(null);
+        $this->assertNotNull( $settings );
+        $settings_fields_with_page_ids = array(
+            'job_manager_submit_job_form_page_id',
+            'job_manager_job_dashboard_page_id',
+            'job_manager_jobs_page_id'
+        );
+        foreach ( $settings_fields_with_page_ids as $field_name ) {
+            $previous_value = $settings->get( $field_name );
+            $settings->set( $field_name, -1 );
+            $result = $settings->validate();
+            $this->assertWPError( $result );
+            $settings->set( $field_name, $previous_value );
+            $this->assertModelValid( $settings );
+        }
+    }
+
+    function test_dto_name_for_field_removes_job_manager_prefix() {
+        $setting_definition = $this->environment()
+            ->get()
+            ->model( WP_Job_Manager_Models_Settings::class );
+        $settings = $setting_definition->get_data_store()->get_entity(null);
+        $this->assertNotNull( $settings );
+        $dto = $setting_definition->model_to_dto( $settings );
+        $this->assertInternalType( 'array', $dto );
+        $this->assertArrayHasKey( 'per_page', $dto );
+        foreach ( $dto as $key => $value ) {
+            $this->assertNotContains( 'job_manager_', $key );
+        }
+    }
+}
