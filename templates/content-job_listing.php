fef72636f13ec47e72001fe9e614dc83b10e3e82
Spencer Finnell <spencerfinnell@gmail.com>
2017-06-06 11:33:32 -0400

    Output multiple job types in template files. See #854

--- templates/content-job_listing.php
+++ templates/content-job_listing.php
@@ -1,4 +1,17 @@
-<?php global $post; ?>
+<?php
+/**
+ * Job listing in the loop.
+ *
+ * @since 1.0.0
+ * @version 1.26.2
+ *
+ * @package WP Job Manager
+ * @category Template
+ * @author Automattic
+ */
+
+global $post; ?>
+
 <li <?php job_listing_class(); ?> data-longitude="<?php echo esc_attr( $post->geolocation_lat ); ?>" data-latitude="<?php echo esc_attr( $post->geolocation_long ); ?>">
 	<a href="<?php the_job_permalink(); ?>">
 		<?php the_company_logo(); ?>
@@ -16,8 +29,21 @@
 			<?php do_action( 'job_listing_meta_start' ); ?>
 
 			<?php if ( get_option( 'job_manager_enable_types' ) ) { ?>
-			<li class="job-type <?php echo get_the_job_type() ? sanitize_title( get_the_job_type()->slug ) : ''; ?>"><?php the_job_type(); ?></li>
+				<?php if ( ! get_option( 'job_manager_multi_job_type', false ) ) : ?>
+
+					<li class="job-type <?php echo get_the_job_type() ? esc_attr( sanitize_title( get_the_job_type()->slug ) ) : ''; ?>" itemprop="employmentType"><?php the_job_type(); ?></li>
+
+				<?php else : $types = get_the_job_types(); ?>
+
+					<?php if ( ! empty( $types ) ) : foreach ( $types as $type ) : ?>
+
+						<li class="job-type <?php echo esc_attr( sanitize_title( $type->slug ) ); ?>" itemprop="employmentType"><?php echo esc_html( $type->name ); ?></li>
+
+					<?php endforeach; endif; ?>
+
+				<?php endif; ?>
 			<?php } ?>
+
 			<li class="date"><?php the_job_publish_date(); ?></li>
 
 			<?php do_action( 'job_listing_meta_end' ); ?>
