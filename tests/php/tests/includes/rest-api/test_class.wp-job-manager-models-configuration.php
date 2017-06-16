1e954f7d923d7c0815295323dddc31ddd8c99c3f
Panos Kountanis <panosktn@gmail.com>
2017-06-16 17:18:36 +0300

    Rename endpoint to status

--- tests/php/tests/includes/rest-api/test_class.wp-job-manager-models-configuration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class WP_Test_WP_Job_Manager_Models_Configuration extends WPJM_REST_TestCase {
-	function test_exists() {
-		$this->assertClassExists( 'WP_Job_Manager_Models_Configuration' );
-	}
-}
