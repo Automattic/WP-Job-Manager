4f7fd2df4ff31f9fe7f6c16d55145e325a2ddc2b
Panos Kountanis <panosktn@gmail.com>
2017-06-16 17:18:36 +0300

    Rename endpoint to status

--- tests/php/tests/includes/rest-api/test_class.wp-job-manager-controllers-status.php
+++ tests/php/tests/includes/rest-api/test_class.wp-job-manager-controllers-status.php
@@ -1,6 +1,6 @@
 <?php
 
-class WP_Test_WP_Job_Manager_Controllers_Configuration extends WPJM_REST_TestCase {
+class WP_Test_WP_Job_Manager_Controllers_Status extends WPJM_REST_TestCase {
 
 	/**
 	 * @var int
@@ -38,13 +38,13 @@ class WP_Test_WP_Job_Manager_Controllers_Configuration extends WPJM_REST_TestCas
 
 	function test_get_succeed_when_user_not_admin() {
 		$this->login_as( $this->default_user_id );
-		$response = $this->get( '/wpjm/v1/configuration' );
+		$response = $this->get( '/wpjm/v1/status' );
 		$this->assertResponseStatus( $response, 200 );
 	}
 
 	function test_get_index_response() {
 		$this->login_as( $this->default_user_id );
-		$response = $this->get( '/wpjm/v1/configuration' );
+		$response = $this->get( '/wpjm/v1/status' );
 		$this->assertResponseStatus( $response, 200 );
 		$data = $response->get_data();
 		$this->assertArrayHasKey( 'run_page_setup', $data );
@@ -53,7 +53,7 @@ class WP_Test_WP_Job_Manager_Controllers_Configuration extends WPJM_REST_TestCas
 
 	function test_get_show_response_succeed_when_valid_key() {
 		$this->login_as( $this->default_user_id );
-		$response = $this->get( '/wpjm/v1/configuration/run_page_setup' );
+		$response = $this->get( '/wpjm/v1/status/run_page_setup' );
 		$this->assertResponseStatus( $response, 200 );
 		$data = $response->get_data();
 		$this->assertInternalType( 'bool', $data );
@@ -61,29 +61,29 @@ class WP_Test_WP_Job_Manager_Controllers_Configuration extends WPJM_REST_TestCas
 
 	function test_get_show_response_not_found_when_valid_key() {
 		$this->login_as( $this->default_user_id );
-		$response = $this->get( '/wpjm/v1/configuration/invalid' );
+		$response = $this->get( '/wpjm/v1/status/invalid' );
 		$this->assertResponseStatus( $response, 404 );
 	}
 
 	function test_delete_not_found() {
-		$response = $this->delete( '/wpjm/v1/configuration/run_page_setup' );
+		$response = $this->delete( '/wpjm/v1/status/run_page_setup' );
 		$this->assertResponseStatus( $response, 404 );
 	}
 
 	function test_post_created_key_value_from_request_body() {
-		$response = $this->post( '/wpjm/v1/configuration/run_page_setup', 'true' );
+		$response = $this->post( '/wpjm/v1/status/run_page_setup', 'true' );
 		$this->assertResponseStatus( $response, 201 );
 	}
 
 	function test_post_created_key_value_from_value_param() {
-		$response = $this->post( '/wpjm/v1/configuration/run_page_setup', array(
+		$response = $this->post( '/wpjm/v1/status/run_page_setup', array(
 			'value' => true,
 		) );
 		$this->assertResponseStatus( $response, 201 );
 	}
 
 	function test_put_ok_key_value_from_value_param() {
-		$response = $this->put( '/wpjm/v1/configuration/run_page_setup', array(
+		$response = $this->put( '/wpjm/v1/status/run_page_setup', array(
 			'value' => true,
 		) );
 		$this->assertResponseStatus( $response, 200 );
@@ -91,15 +91,15 @@ class WP_Test_WP_Job_Manager_Controllers_Configuration extends WPJM_REST_TestCas
 
 	function test_put_updates_key_value_from_value_param() {
 		$value = $this->environment()
-			->model( 'WP_Job_Manager_Models_Configuration' )
+			->model( 'WP_Job_Manager_Models_Status' )
 			->get_data_store()->get_entity( '' )
 			->get( 'run_page_setup' );
-		$response = $this->put( '/wpjm/v1/configuration/run_page_setup', array(
+		$response = $this->put( '/wpjm/v1/status/run_page_setup', array(
 			'value' => ! $value,
 		) );
 		$this->assertResponseStatus( $response, 200 );
 		$model = $this->environment()
-			->model( 'WP_Job_Manager_Models_Configuration' )
+			->model( 'WP_Job_Manager_Models_Status' )
 			->get_data_store()->get_entity( '' );
 		$this->assertNotEquals( $value, $model->get( 'run_page_setup' ) );
 	}
@@ -107,7 +107,7 @@ class WP_Test_WP_Job_Manager_Controllers_Configuration extends WPJM_REST_TestCas
 	function test_post_response_status_requires_admin() {
 		$this->login_as( $this->default_user_id );
 
-		$response = $this->put( '/wpjm/v1/configuration/run_page_setup', array(
+		$response = $this->put( '/wpjm/v1/status/run_page_setup', array(
 			'value' => false,
 		) );
 
