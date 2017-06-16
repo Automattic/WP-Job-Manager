2492911ffd61d9dbc09193753257bbf033905636
Panos Kountanis <panosktn@gmail.com>
2017-06-16 16:14:15 +0300

    minor fixes in controller, phpunit

--- includes/rest-api/class-wp-job-manager-controllers-configuration.php
+++ includes/rest-api/class-wp-job-manager-controllers-configuration.php
@@ -35,12 +35,13 @@ class WP_Job_Manager_Controllers_Configuration extends WPJM_REST_Controller_Mode
 	 * @return WP_REST_Response
 	 */
 	function index( $request ) {
+		$params = $request->get_params();
 		$filter = $this->environment()
 			->model( 'WP_Job_Manager_Filters_Configuration' )
-			->new_from_array( $request->get_params() );
+			->new_from_array( $params );
 
 		if ( is_wp_error( $filter ) ) {
-			$this->bad_request( $filter );
+			return $this->bad_request( $filter );
 		}
 
 		$configuration = $this->get_model_definition()
@@ -95,7 +96,11 @@ class WP_Job_Manager_Controllers_Configuration extends WPJM_REST_Controller_Mode
 		$key = $request->get_param( 'key' );
 		$value = $request->get_param( 'value' );
 		if ( empty( $value ) ) {
-			$value = json_decode( $request->get_body(), true );
+			if ( ! function_exists( 'json_decode' ) ) {
+				include_once ABSPATH . WPINC . 'compat.php';
+			}
+			$body = $request->get_body();
+			$value = json_decode( $body, true );
 		}
 		$thing_to_update = array(
 		 $key => $value,
@@ -111,14 +116,16 @@ class WP_Job_Manager_Controllers_Configuration extends WPJM_REST_Controller_Mode
 			->upsert( $configuration );
 
 		if ( is_wp_error( $result ) ) {
-			   return $this->respond( $result, 500 );
+			return $this->respond( $result, 500 );
 		}
 
 		$dto = $this->prepare_dto( $configuration );
 
-		return WP_REST_Server::CREATABLE === $request->get_method() ?
-		 $this->created( $dto ) :
-		 $this->ok( $dto );
+		if ( WP_REST_Server::CREATABLE === $request->get_method() ) {
+			return $this->created( $dto );
+		}
+
+		return $this->ok( $dto );
 	}
 }
 
