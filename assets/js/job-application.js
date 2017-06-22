b7c12baf288e5196311d883ebb84eca91055f5dd
Jake Oehler Morrison <jake.morrison@automattic.com>
2017-06-22 12:06:40 +0100

    Allow people to force the job application details to remain open
    ```
    add_filter( 'body_class', function( $body_class ) {
    	$body_class[] = 'job-application-details-keep-open';
    	return $body_class;
    } );
    ```

--- assets/js/job-application.js
+++ assets/js/job-application.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function($) {
 	// Slide toggle
-	$( '.application_details' ).hide();
+	if ( ! $( 'body' ).hasClass( 'job-application-details-keep-open' ) ) {
+		$('.application_details').hide();
+	}
 	$( '.application_button' ).click(function() {
 		$( '.application_details' ).slideToggle();
 	});
