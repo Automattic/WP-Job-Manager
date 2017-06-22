369157cc5c69fa561ca8ccbbfea288512ebeefba
Jake Oehler Morrison <jake.morrison@automattic.com>
2017-06-22 17:13:07 +0100

    Move deprecated functions to their own file

--- wp-job-manager-template.php
+++ wp-job-manager-template.php
@@ -284,54 +284,6 @@ function wpjm_get_the_job_title( $post = null ) {
 }
 
 /**
- * Displays the job type for the listing.
- *
- * @since 1.0.0
- * @deprecated 1.26.3 Use `wpjm_the_job_types()` instead.
- *
- * @param int|WP_Post $post
- * @return string
- */
-function the_job_type( $post = null ) {
-	_deprecated_function( __FUNCTION__, '1.26.3', 'wpjm_the_job_types' );
-
-	if ( ! get_option( 'job_manager_enable_types' ) ) {
-		return '';
-	}
-	if ( $job_type = get_the_job_type( $post ) ) {
-		echo $job_type->name;
-	}
-}
-
-/**
- * Gets the job type for the listing.
- *
- * @since 1.0.0
- * @deprecated 1.26.3 Use `wpjm_get_the_job_types()` instead.
- *
- * @param int|WP_Post $post (default: null)
- * @return string|bool|null
- */
-function get_the_job_type( $post = null ) {
-	_deprecated_function( __FUNCTION__, '1.26.3', 'wpjm_get_the_job_types' );
-
-	$post = get_post( $post );
-	if ( $post->post_type !== 'job_listing' ) {
-		return;
-	}
-
-	$types = wp_get_post_terms( $post->ID, 'job_listing_type' );
-
-	if ( $types ) {
-		$type = current( $types );
-	} else {
-		$type = false;
-	}
-
-	return apply_filters( 'the_job_type', $type, $post );
-}
-
-/**
  * Displays multiple job types for the listing.
  *
  * @since 1.26.3
