e13d1083eac4f1008f8490289d72b902359e20a4
Bernhard Reiter <ockham@raz.or.at>
2017-07-21 15:40:44 +0200

    Install & enable page

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-// import page from 'page';
+import page from 'page';
 import { get } from 'lodash';
 
 /**
@@ -24,11 +24,11 @@ export default function() {
 	const pagesSlug = get( Tabs, 'PAGES.slug', '' );
 
 	// page( '/extensions/wp-job-manager', sites );
-	// page( '/extensions/wp-job-manager/:site', siteSelection, navigation, renderTab( JobListings ) );
-	// page( `/extensions/wp-job-manager/${ jobSubmissionSlug }/:site`, siteSelection, navigation,
-	// 	renderTab( JobSubmission, jobSubmissionSlug ) );
-	// page( `/extensions/wp-job-manager/${ pagesSlug }/:site`, siteSelection, navigation,
-	// 	renderTab( Pages, pagesSlug ) );
+	page( '/extensions/wp-job-manager/:site', siteSelection, navigation, renderTab( JobListings ) );
+	page( `/extensions/wp-job-manager/${ jobSubmissionSlug }/:site`, siteSelection, navigation,
+		renderTab( JobSubmission, jobSubmissionSlug ) );
+	page( `/extensions/wp-job-manager/${ pagesSlug }/:site`, siteSelection, navigation,
+	 	renderTab( Pages, pagesSlug ) );
 }
 
 // initExtension();
