ddbad8a168413f13b3c66d9fdea66ed6e8bd1d93
Bernhard Reiter <ockham@raz.or.at>
2017-07-25 15:57:42 +0200

    Disable analytics

--- js/app/controller.js
+++ js/app/controller.js
@@ -6,32 +6,32 @@ import React from 'react';
 /**
  * Internal Dependencies
  */
-import analytics from 'lib/analytics';
+//import analytics from 'lib/analytics';
 import titlecase from 'to-title-case';
-import { getSiteFragment, sectionify } from 'lib/route';
+//import { getSiteFragment, sectionify } from 'lib/route';
 import { renderWithReduxStore } from 'lib/react-helpers';
 import Settings from '../components/settings';
 
 export const renderTab = ( component, tab = '' ) => ( context ) => {
-	const siteId = getSiteFragment( context.path );
-	const basePath = sectionify( context.path );
-	let baseAnalyticsPath;
-
-	if ( siteId ) {
-		baseAnalyticsPath = `${ basePath }/:site`;
-	} else {
-		baseAnalyticsPath = basePath;
-	}
-
-	let analyticsPageTitle = 'WP Job Manager';
-
-	if ( tab.length ) {
-		analyticsPageTitle += ` > ${ titlecase( tab ) }`;
-	} else {
-		analyticsPageTitle += ' > Job Listings';
-	}
-
-	analytics.pageView.record( baseAnalyticsPath, analyticsPageTitle );
+	// const siteId = getSiteFragment( context.path );
+	// const basePath = sectionify( context.path );
+	// let baseAnalyticsPath;
+	//
+	// if ( siteId ) {
+	// 	baseAnalyticsPath = `${ basePath }/:site`;
+	// } else {
+	// 	baseAnalyticsPath = basePath;
+	// }
+	//
+	// let analyticsPageTitle = 'WP Job Manager';
+	//
+	// if ( tab.length ) {
+	// 	analyticsPageTitle += ` > ${ titlecase( tab ) }`;
+	// } else {
+	// 	analyticsPageTitle += ' > Job Listings';
+	// }
+	//
+	// analytics.pageView.record( baseAnalyticsPath, analyticsPageTitle );
 
 	renderWithReduxStore(
 		<Settings tab={ tab }>
