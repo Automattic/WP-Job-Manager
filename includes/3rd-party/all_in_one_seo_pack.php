c44b411f35ebb3e8b542f25c67d75874beddf44a
Jake Oehler Morrison <jake.morrison@automattic.com>
2017-07-27 15:52:37 +0100

    Add support for All in One SEO Pack

---
+++ includes/3rd-party/all_in_one_seo_pack.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Adds additional compatibility with All in One SEO Pack.
+ */
+
+/**
+ * Skip filled job listings.
+ *
+ * @param WP_Post[] $posts
+ * @return WP_Post[]
+ */
+function wpjm_aiosp_sitemap_filter_filled_jobs( $posts ) {
+	foreach ( $posts as $index => $post ) {
+		if ( $post instanceof WP_Post && 'job_listing' !== $post->post_type ) {
+			continue;
+		}
+		if ( is_position_filled( $post ) ) {
+			unset( $posts[ $index ] );
+		}
+	}
+	return $posts;
+}
+add_action( 'aiosp_sitemap_post_filter', 'wpjm_aiosp_sitemap_filter_filled_jobs', 10, 3 );
