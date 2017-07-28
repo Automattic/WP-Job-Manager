3bcebe88050545a07fa6ec16d5e9aa85340bdc26
Jake Oehler Morrison <jake.morrison@automattic.com>
2017-07-28 18:14:20 +0100

    Revert "WPML: Add `lang` to query string for Ajax"
    This reverts commit a6bbab39d37dae5dec18818375ca8390e5a40c2f.

--- includes/3rd-party/wpml.php
+++ includes/3rd-party/wpml.php
@@ -9,11 +9,22 @@
  * @since 1.26.0
  */
 function wpml_wpjm_init() {
+	add_action( 'get_job_listings_init', 'wpml_wpjm_set_language' );
 	add_filter( 'wpjm_lang', 'wpml_wpjm_get_job_listings_lang' );
-	add_filter( 'wpjm_ajax_endpoint', 'wpml_wpjm_add_lang_to_ajax_endpoint' );
 	add_filter( 'wpjm_page_id', 'wpml_wpjm_page_id' );
 }
 add_action( 'wpml_loaded', 'wpml_wpjm_init' );
+
+/**
+ * Sets WPJM's language if it is sent in the Ajax request.
+ *
+ * @since 1.26.0
+ */
+function wpml_wpjm_set_language() {
+	if ( ( strstr( $_SERVER['REQUEST_URI'], '/jm-ajax/' ) || ! empty( $_GET['jm-ajax'] ) ) && isset( $_POST['lang'] ) ) {
+		do_action( 'wpml_switch_language', sanitize_text_field( $_POST['lang'] ) );
+	}
+}
 
 /**
  * Returns WPML's current language.
@@ -30,27 +41,9 @@ function wpml_wpjm_get_job_listings_lang( $lang ) {
 /**
  * Returns the page ID for the current language.
  *
- * @since 1.26.0
- *
  * @param int $page_id
  * @return int
  */
 function wpml_wpjm_page_id( $page_id ) {
 	return apply_filters( 'wpml_object_id', $page_id, 'page', true );
-}
-
-/**
- * Add language to ajax endpoint.
- *
- * @since 1.28.0
- *
- * @param string $endpoint
- * @return string
- */
-function wpml_wpjm_add_lang_to_ajax_endpoint( $endpoint ) {
-	$lang = apply_filters( 'wpml_current_language', null );
-	if ( ! empty( $lang ) ) {
-		$endpoint = add_query_arg( 'lang', $lang, $endpoint );
-	}
-	return $endpoint;
 }
