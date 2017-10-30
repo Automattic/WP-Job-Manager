e4e2cb4cbe78a398025716949bbf240170667d72
Panos Kountanis <panosktn@gmail.com>
2017-10-30 16:11:08 +0200

    refs #1182. REST API: Enable Job Types Endpoint

--- includes/rest-api/class-wp-job-manager-rest-api.php
+++ includes/rest-api/class-wp-job-manager-rest-api.php
@@ -88,7 +88,9 @@ class WP_Job_Manager_REST_API {
 		include_once 'class-wp-job-manager-data-stores-status.php';
 		include_once 'class-wp-job-manager-controllers-status.php';
 		include_once 'class-wp-job-manager-models-job-listings-custom-fields.php';
+		include_once 'class-wp-job-manager-models-job-types-custom-fields.php';
 		include_once 'class-wp-job-manager-registrable-job-listings.php';
+		include_once 'class-wp-job-manager-registrable-job-types.php';
 
 		// Models.
 		$env->define_model( 'WP_Job_Manager_Models_Settings' )
@@ -97,6 +99,7 @@ class WP_Job_Manager_REST_API {
 			->with_data_store( new WP_Job_Manager_Data_Stores_Status( $env->model( 'WP_Job_Manager_Models_Status' ) ) );
 		$env->define_model( 'WP_Job_Manager_Filters_Status' );
 		$env->define_model( 'WP_Job_Manager_Models_Job_Listings_Custom_Fields' );
+		$env->define_model( 'WP_Job_Manager_Models_Job_Types_Custom_Fields' );
 
 		// Endpoints.
 		$env->rest_api( 'wpjm/v1' )
@@ -106,6 +109,7 @@ class WP_Job_Manager_REST_API {
 			'job_listing',
 			'WP_Job_Manager_Models_Job_Listings_Custom_Fields',
 			'fields' ) );
+		$env->add_registrable( new WP_Job_Manager_Registrable_Job_Types() );
 	}
 }
 
