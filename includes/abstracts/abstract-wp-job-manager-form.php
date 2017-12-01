f3c3f403b2fcffdb6bbeed5505aa5f96696a1fad
albion.selimaj.4@gmail.com <albion.selimaj.4@gmail.com>
2017-12-01 14:48:36 +0100

    PHP 5.2 compatible fix for 'sanitize_posted_field' issue

--- includes/abstracts/abstract-wp-job-manager-form.php
+++ includes/abstracts/abstract-wp-job-manager-form.php
@@ -294,10 +294,13 @@ abstract class WP_Job_Manager_Form {
 	protected function sanitize_posted_field( $value, $sanitizer = null ) {
 		// Sanitize value
 		if ( is_array( $value ) ) {
-			return array_map( function( $value ) use ( $sanitizer ) {
-				return $this->sanitize_posted_field( $value, $sanitizer );
-			}, $value );
+			foreach ($value as $key => $val) {
+				$value[ $key ] = $this->sanitize_posted_field( $val, $sanitizer );
+			}
+
+			return $value;
 		}
+
 		$value = trim( $value );
 
 		if ( 'url' === $sanitizer ) {
