09f86cdfb717c7e19582531852e669d0c71bb9b5
Jake Oehler Morrison <jake.morrison@automattic.com>
2017-12-04 13:56:41 +0000

    Bump version to 1.29.2

--- wp-job-manager.php
+++ wp-job-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Job Manager
  * Plugin URI: https://wpjobmanager.com/
  * Description: Manage job listings from the WordPress admin panel, and allow users to post jobs directly to your site.
- * Version: 1.29.1
+ * Version: 1.29.2
  * Author: Automattic
  * Author URI: https://wpjobmanager.com/
  * Requires at least: 4.1
@@ -58,7 +58,7 @@ class WP_Job_Manager {
 	 */
 	public function __construct() {
 		// Define constants
-		define( 'JOB_MANAGER_VERSION', '1.29.1' );
+		define( 'JOB_MANAGER_VERSION', '1.29.2' );
 		define( 'JOB_MANAGER_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 		define( 'JOB_MANAGER_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
 
