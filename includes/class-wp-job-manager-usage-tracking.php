6641b2542326f7fa101697d7bf1e1cb547b6c84b
Alex Sanford <alex.sanford1@gmail.com>
2018-02-12 14:00:08 -0400

    Add Usage Tracking setting

--- includes/class-wp-job-manager-usage-tracking.php
+++ includes/class-wp-job-manager-usage-tracking.php
@@ -11,7 +11,16 @@ include dirname( __FILE__ ) . '/../lib/usage-tracking/class-usage-tracking-base.
  **/
 class WP_Job_Manager_Usage_Tracking extends WP_Job_Manager_Usage_Tracking_Base {
 
+	const WPJM_SETTING_NAME = 'job_manager_usage_tracking_enabled';
+
 	const WPJM_TRACKING_INFO_URL = 'https://wpjobmanager.com/document/what-data-does-wpjm-track';
+
+	protected function __construct() {
+		parent::__construct();
+
+		// Add filter for settings.
+		add_filter( 'job_manager_settings', array( $this, 'add_setting_field' ) );
+	}
 
 	/*
 	 * Implementation for abstract functions.
@@ -26,11 +35,11 @@ class WP_Job_Manager_Usage_Tracking extends WP_Job_Manager_Usage_Tracking_Base {
 	}
 
 	protected function get_tracking_enabled() {
-		// TODO
+		return get_option( self::WPJM_SETTING_NAME  ) || false;
 	}
 
 	protected function set_tracking_enabled( $enable ) {
-		// TODO
+		update_option( self::WPJM_SETTING_NAME, $enable );
 	}
 
 	protected function current_user_can_manage_tracking() {
@@ -42,5 +51,29 @@ class WP_Job_Manager_Usage_Tracking extends WP_Job_Manager_Usage_Tracking_Base {
 			<a href=\"%s\" target=\"_blank\">usage tracking data</a>.
 			No sensitive information is collected, and you can opt out at any time.",
 			'wp-job-manager' ), self::WPJM_TRACKING_INFO_URL );
+	}
+
+
+	/*
+	 * Hooks.
+	 */
+
+	public function add_setting_field( $fields ) {
+		$fields['general'][1][] = array(
+			'name'     => self::WPJM_SETTING_NAME,
+			'std'      => '0',
+			'type'     => 'checkbox',
+			'desc'     => '',
+			'label'    => __( 'Enable usage tracking', 'wp-job-manager' ),
+			'cb_label' => sprintf(
+				__(
+					'Help us make WP Job Manager better by allowing us to collect
+					<a href="%s" target="_blank">usage tracking data</a>.
+					No sensitive information is collected.', 'wp-job-manager'
+				), self::WPJM_TRACKING_INFO_URL
+			),
+		);
+
+		return $fields;
 	}
 }
