2be9caf6494cad8823d58ad7925bbd2c393a65cf
Alex Sanford <alex.sanford1@gmail.com>
2018-02-13 16:38:07 -0400

    Add usage tracking data for number of Employers (#1325)

--- includes/class-wp-job-manager-usage-tracking-data.php
+++ includes/class-wp-job-manager-usage-tracking-data.php
@@ -23,6 +23,21 @@ class WP_Job_Manager_Usage_Tracking_Data {
 	public static function get_usage_data() {
 		return array(
 			'jobs' => wp_count_posts( 'job_listing' )->publish,
+			'employers' => self::get_employer_count(),
 		);
+	}
+
+	/**
+	 * Get the total number of users with the "employer" role.
+	 *
+	 * @return int the number of "employers".
+	 */
+	private static function get_employer_count() {
+		$employer_query = new WP_User_Query( array(
+			'fields' => 'ID',
+			'role' => 'employer',
+		) );
+
+		return $employer_query->total_users;
 	}
 }
