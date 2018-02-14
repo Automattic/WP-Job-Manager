d5037d4124f419a92b0c052ab2dfbad0a0ebe037
Donna Peplinskie <donnapep@gmail.com>
2018-02-14 17:10:19 -0500

    Add count of jobs with company logo and job type

--- includes/class-wp-job-manager-usage-tracking-data.php
+++ includes/class-wp-job-manager-usage-tracking-data.php
@@ -9,12 +9,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Supplies the usage tracking data for logging.
- *
- * @package Usage Tracking
- * @since 1.30.0
- */
+	/**
+	 * Supplies the usage tracking data for logging.
+	 *
+	 * @package Usage Tracking
+	 * @since 1.30.0
+	 */
 class WP_Job_Manager_Usage_Tracking_Data {
 	/**
 	 * Get the usage tracking data to send.
@@ -28,6 +28,8 @@ class WP_Job_Manager_Usage_Tracking_Data {
 
 		return array(
 			'employers'            => self::get_employer_count(),
+			'jobs_type'            => self::get_job_type_count(),
+			'jobs_logo'            => self::get_company_logo_count(),
 			'jobs_expired'         => isset( $count_posts->expired ) ? $count_posts->expired : 0,
 			'jobs_pending'         => $count_posts->pending,
 			'jobs_pending_payment' => isset( $count_posts->pending_payment ) ? $count_posts->pending_payment : 0,
@@ -50,5 +52,55 @@ class WP_Job_Manager_Usage_Tracking_Data {
 		);
 
 		return $employer_query->total_users;
+	}
+
+	/**
+	 * Get the number of job listings that have a company logo.
+	 *
+	 * @since 1.30.0
+	 *
+	 * @return int Number of job listings with a company logo.
+	 */
+	private static function get_company_logo_count() {
+		$query = new WP_Query(
+			array(
+				'post_type'   => 'job_listing',
+				'post_status' => array( 'expired', 'publish' ),
+				'fields'      => 'ids',
+				'meta_query'  => array(
+					array(
+						'key'     => '_thumbnail_id',
+						'compare' => 'EXISTS',
+					),
+				),
+			)
+		);
+
+		return $query->found_posts;
+	}
+
+	/**
+	 * Get the total number of job listings that have one or more job types selected.
+	 *
+	 * @since 1.30.0
+	 *
+	 * @return array Number of job listings associated with at least one job type.
+	 **/
+	private static function get_job_type_count() {
+		$query = new WP_Query(
+			array(
+				'post_type'   => 'job_listing',
+				'post_status' => array( 'expired', 'publish' ),
+				'fields'      => 'ids',
+				'tax_query'   => array(
+					array(
+						'taxonomy' => 'job_listing_type',
+						'operator' => 'EXISTS',
+					),
+				),
+			)
+		);
+
+		return $query->found_posts;
 	}
 }
--- tests/php/tests/includes/test_class.wp-job-manager-usage-tracking-data.php
+++ tests/php/tests/includes/test_class.wp-job-manager-usage-tracking-data.php
@@ -47,27 +47,42 @@ class WP_Test_WP_Job_Manager_Usage_Tracking_Data extends WPJM_BaseTest {
 	private $publish_count = 15;
 
 	/**
+	 * Job listing IDs
+	 *
+	 * @var array
+	 */
+	private $listings;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->create_job_listings();
+	}
+
+	/**
 	 * Create a number of job listings with different statuses.
 	 */
 	private function create_job_listings() {
-		$this->factory->job_listing->create_many(
+		$draft           = $this->factory->job_listing->create_many(
 			2, array( 'post_status' => 'draft' )
 		);
-		$this->factory->job_listing->create_many(
+		$expired         = $this->factory->job_listing->create_many(
 			$this->expired_count, array( 'post_status' => 'expired' )
 		);
-		$this->factory->job_listing->create_many(
+		$preview         = $this->factory->job_listing->create_many(
 			$this->preview_count, array( 'post_status' => 'preview' )
 		);
-		$this->factory->job_listing->create_many(
+		$pending         = $this->factory->job_listing->create_many(
 			$this->pending_count, array( 'post_status' => 'pending' )
 		);
-		$this->factory->job_listing->create_many(
+		$pending_payment = $this->factory->job_listing->create_many(
 			$this->pending_payment_count, array( 'post_status' => 'pending_payment' )
 		);
-		$this->factory->job_listing->create_many(
+		$publish         = $this->factory->job_listing->create_many(
 			$this->publish_count, array( 'post_status' => 'publish' )
 		);
+
+		$this->listings = array_merge( $draft, $expired, $preview, $pending, $pending_payment, $publish );
 	}
 
 	/**
@@ -78,7 +93,7 @@ class WP_Test_WP_Job_Manager_Usage_Tracking_Data extends WPJM_BaseTest {
 	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
 	 */
 	public function test_employers() {
-		$employer_count = 3;
+		$employer_count   = 3;
 		$subscriber_count = 2;
 
 		$this->factory->user->create_many(
@@ -99,8 +114,6 @@ class WP_Test_WP_Job_Manager_Usage_Tracking_Data extends WPJM_BaseTest {
 	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
 	 */
 	public function test_get_usage_data_expired_jobs() {
-		$this->create_job_listings();
-
 		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
 
 		$this->assertEquals( $this->expired_count, $data['jobs_expired'] );
@@ -113,8 +126,6 @@ class WP_Test_WP_Job_Manager_Usage_Tracking_Data extends WPJM_BaseTest {
 	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
 	 */
 	public function test_get_usage_data_pending_jobs() {
-		$this->create_job_listings();
-
 		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
 
 		$this->assertEquals( $this->pending_count, $data['jobs_pending'] );
@@ -127,8 +138,6 @@ class WP_Test_WP_Job_Manager_Usage_Tracking_Data extends WPJM_BaseTest {
 	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
 	 */
 	public function test_get_usage_data_pending_payment_jobs() {
-		$this->create_job_listings();
-
 		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
 
 		$this->assertEquals( $this->pending_payment_count, $data['jobs_pending_payment'] );
@@ -141,8 +150,6 @@ class WP_Test_WP_Job_Manager_Usage_Tracking_Data extends WPJM_BaseTest {
 	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
 	 */
 	public function test_get_usage_data_preview_jobs() {
-		$this->create_job_listings();
-
 		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
 
 		$this->assertEquals( $this->preview_count, $data['jobs_preview'] );
@@ -155,10 +162,62 @@ class WP_Test_WP_Job_Manager_Usage_Tracking_Data extends WPJM_BaseTest {
 	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
 	 */
 	public function test_get_usage_data_publish_jobs() {
-		$this->create_job_listings();
-
 		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
 
 		$this->assertEquals( $this->publish_count, $data['jobs_publish'] );
+	}
+
+	/**
+	 * Jobs with a company logo count.
+	 *
+	 * @since 1.30.0
+	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
+	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_company_logo_count
+	 */
+	public function test_get_company_logo_count() {
+		// Create some media attachments.
+		$media = $this->factory->attachment->create_many(
+			6, array(
+				'post_type'   => 'job_listing',
+				'post_status' => 'publish',
+			)
+		);
+
+		// Add logos to some listings with varying statuses.
+		add_post_meta( $this->listings[0], '_thumbnail_id', $media[0] );
+		add_post_meta( $this->listings[5], '_thumbnail_id', $media[1] );
+		add_post_meta( $this->listings[6], '_thumbnail_id', $media[2] );
+		add_post_meta( $this->listings[12], '_thumbnail_id', $media[3] );
+		add_post_meta( $this->listings[20], '_thumbnail_id', $media[4] );
+		add_post_meta( $this->listings[24], '_thumbnail_id', $media[5] );
+
+		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
+
+		// 2 expired + 1 publish
+		$this->assertEquals( 3, $data['jobs_logo'] );
+	}
+
+	/**
+	 * Jobs with a company logo count.
+	 *
+	 * @since 1.30.0
+	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
+	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_job_type_count
+	 */
+	public function test_get_job_type_count() {
+		$terms = $this->factory->term->create_many( 6, array( 'taxonomy' => 'job_listing_type' ) );
+
+		// Assign job types to some jobs.
+		wp_set_object_terms( $this->listings[0], $terms[0], 'job_listing_type', false );
+		wp_set_object_terms( $this->listings[5], $terms[1], 'job_listing_type', false );
+		wp_set_object_terms( $this->listings[6], $terms[2], 'job_listing_type', false );
+		wp_set_object_terms( $this->listings[12], $terms[3], 'job_listing_type', false );
+		wp_set_object_terms( $this->listings[20], $terms[4], 'job_listing_type', false );
+		wp_set_object_terms( $this->listings[24], $terms[5], 'job_listing_type', false );
+
+		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
+
+		// 2 expired + 1 publish
+		$this->assertEquals( 3, $data['jobs_type'] );
 	}
 }
