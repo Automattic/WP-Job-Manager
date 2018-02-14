06722dc0bbf57fdabe529c19ef5b38c67ff5b7c7
Donna Peplinskie <donnapep@gmail.com>
2018-02-14 09:35:29 -0500

    Add job counts by status

--- includes/class-wp-job-manager-usage-tracking-data.php
+++ includes/class-wp-job-manager-usage-tracking-data.php
@@ -21,9 +21,15 @@ class WP_Job_Manager_Usage_Tracking_Data {
 	 * @return array Usage data.
 	 **/
 	public static function get_usage_data() {
+		$count_posts = wp_count_posts( 'job_listing' );
+
 		return array(
-			'jobs_published' => wp_count_posts( 'job_listing' )->publish,
-			'employers' => self::get_employer_count(),
+			'employers'            => self::get_employer_count(),
+			'jobs_expired'         => isset( $count_posts->expired ) ? $count_posts->expired : 0,
+			'jobs_pending'         => $count_posts->pending,
+			'jobs_pending_payment' => isset( $count_posts->pending_payment ) ? $count_posts->pending_payment : 0,
+			'jobs_preview'         => isset( $count_posts->preview ) ? $count_posts->preview : 0,
+			'jobs_publish'         => $count_posts->publish,
 		);
 	}
 
--- tests/php/tests/includes/test_class.wp-job-manager-usage-tracking-data.php
+++ tests/php/tests/includes/test_class.wp-job-manager-usage-tracking-data.php
@@ -1,26 +1,31 @@
 <?php
 
 class WP_Test_WP_Job_Manager_Usage_Tracking_Data extends WPJM_BaseTest {
-	/**
-	 * Tests that get_usage_data() returns the correct number of published job
-	 * listings.
-	 *
-	 * @since 1.30.0
-	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
-	 */
-	public function test_job_listings() {
-		$published_listing_count = 3;
-		$draft_listing_count = 2;
+	private $expired_count         = 10;
+	private $preview_count         = 1;
+	private $pending_count         = 8;
+	private $pending_payment_count = 3;
+	private $publish_count         = 15;
 
+	private function create_job_listings() {
 		$this->factory->job_listing->create_many(
-			$published_listing_count, array( 'post_status' => 'publish' )
+			2, array( 'post_status' => 'draft' )
 		);
 		$this->factory->job_listing->create_many(
-			$draft_listing_count, array( 'post_status' => 'draft' )
+			$expired_count, array( 'post_status' => 'expired' )
 		);
-
-		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
-		$this->assertEquals( $published_listing_count, $data['jobs_published'] );
+		$this->factory->job_listing->create_many(
+			$preview_count, array( 'post_status' => 'preview' )
+		);
+		$this->factory->job_listing->create_many(
+			$pending_count, array( 'post_status' => 'pending' )
+		);
+		$this->factory->job_listing->create_many(
+			$pending_payment_count, array( 'post_status' => 'pending_payment' )
+		);
+		$this->factory->job_listing->create_many(
+			$publish_count, array( 'post_status' => 'publish' )
+		);
 	}
 
 	/**
@@ -43,5 +48,75 @@ class WP_Test_WP_Job_Manager_Usage_Tracking_Data extends WPJM_BaseTest {
 
 		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
 		$this->assertEquals( $employer_count, $data['employers'] );
+	}
+
+	/**
+	 * Expired jobs count.
+	 *
+	 * @since 1.30.0
+	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
+	 */
+	public function test_get_usage_data_expired_jobs() {
+		$this->create_job_listings();
+
+		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
+
+		$this->assertEquals( $expired_count, $data['jobs_expired'] );
+	}
+
+	/**
+	 * Pending jobs count.
+	 *
+	 * @since 1.30.0
+	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
+	 */
+	public function test_get_usage_data_pending_jobs() {
+		$this->create_job_listings();
+
+		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
+
+		$this->assertEquals( $pending_count, $data['jobs_pending'] );
+	}
+
+	/**
+	 * Pending payment jobs count.
+	 *
+	 * @since 1.30.0
+	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
+	 */
+	public function test_get_usage_data_pending_payment_jobs() {
+		$this->create_job_listings();
+
+		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
+
+		$this->assertEquals( $pending_payment_count, $data['jobs_pending_payment'] );
+	}
+
+	/**
+	 * Preview jobs count.
+	 *
+	 * @since 1.30.0
+	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
+	 */
+	public function test_get_usage_data_preview_jobs() {
+		$this->create_job_listings();
+
+		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
+
+		$this->assertEquals( $preview_count, $data['jobs_preview'] );
+	}
+
+	/**
+	 * Published jobs count.
+	 *
+	 * @since 1.30.0
+	 * @covers WP_Job_Manager_Usage_Tracking_Data::get_usage_data
+	 */
+	public function test_get_usage_data_publish_jobs() {
+		$this->create_job_listings();
+
+		$data = WP_Job_Manager_Usage_Tracking_Data::get_usage_data();
+
+		$this->assertEquals( $publish_count, $data['jobs_publish'] );
 	}
 }
