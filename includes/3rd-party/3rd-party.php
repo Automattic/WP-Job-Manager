0cc5de425a39a52367d1a4418406ea2e030453c4
Jake Oehler Morrison <jake.morrison@automattic.com>
2018-02-16 15:35:59 +0000

    Rename AIOSEO 3rd party file to match standards

--- includes/3rd-party/3rd-party.php
+++ includes/3rd-party/3rd-party.php
@@ -6,4 +6,4 @@ require_once( JOB_MANAGER_PLUGIN_DIR . '/includes/3rd-party/jetpack.php' );
 require_once( JOB_MANAGER_PLUGIN_DIR . '/includes/3rd-party/wpml.php' );
 require_once( JOB_MANAGER_PLUGIN_DIR . '/includes/3rd-party/polylang.php' );
 require_once( JOB_MANAGER_PLUGIN_DIR . '/includes/3rd-party/yoast.php' );
-require_once( JOB_MANAGER_PLUGIN_DIR . '/includes/3rd-party/all_in_one_seo_pack.php' );
+require_once( JOB_MANAGER_PLUGIN_DIR . '/includes/3rd-party/all-in-one-seo-pack.php' );
