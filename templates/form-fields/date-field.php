a9f086b693bb496814f82a3d2580d2c486d91600
Alex Sanford <alex.sanford1@gmail.com>
2018-02-22 13:11:25 -0400

    Only enqueue jquery-datepicker on the frontend if a date field is present

--- templates/form-fields/date-field.php
+++ templates/form-fields/date-field.php
@@ -14,6 +14,9 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+
+wp_enqueue_script( 'wp-job-manager-datepicker' );
+
 ?>
 <input type="text" class="input-date job-manager-datepicker" name="<?php echo esc_attr( isset( $field['name'] ) ? $field['name'] : $key ); ?>"<?php if ( isset( $field['autocomplete'] ) && false === $field['autocomplete'] ) { echo ' autocomplete="off"'; } ?> id="<?php echo esc_attr( $key ); ?>" placeholder="<?php echo empty( $field['placeholder'] ) ? '' : esc_attr( $field['placeholder'] ); ?>" value="<?php echo isset( $field['value'] ) ? esc_attr( $field['value'] ) : ''; ?>" <?php if ( ! empty( $field['required'] ) ) echo 'required'; ?> />
 <?php if ( ! empty( $field['description'] ) ) : ?><small class="description"><?php echo $field['description']; ?></small><?php endif; ?>
