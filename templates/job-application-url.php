6d09c0a1b09813e7ce23c21c2bac8c5fdedb0713
Alex Sanford <alex.sanford1@gmail.com>
2018-04-17 12:42:52 -0300

    Simplify the application URL (#1456)
    * Simplify the application URL
    
    * Bump template version

--- templates/job-application-url.php
+++ templates/job-application-url.php
@@ -8,11 +8,11 @@
  * @author      Automattic
  * @package     WP Job Manager
  * @category    Template
- * @version     1.9.0
+ * @version     1.31.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-<p><?php _e( 'To apply for this job please visit the following URL:', 'wp-job-manager' ); ?> <a href="<?php echo esc_url( $apply->url ); ?>" target="_blank" rel="nofollow"><?php echo esc_html( $apply->url ); ?> &rarr;</a></p>
+<p><?php esc_html_e( 'To apply for this job please visit', 'wp-job-manager' ); ?> <a href="<?php echo esc_url( $apply->url ); ?>" target="_blank" rel="nofollow"><?php echo esc_html( wp_parse_url( $apply->url, PHP_URL_HOST ) ); ?></a>.</p>
