f95217b6b81ca2a691d66753346cd71d4fc3a1ee
RajeebTheGreat <rajeebthegreat@gmail.com>
2018-05-28 15:21:54 -0500

    Add style for logo in Recent Jobs widget

--- templates/content-widget-job_listing.php
+++ templates/content-widget-job_listing.php
@@ -17,18 +17,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <li <?php job_listing_class(); ?>>
 	<a href="<?php the_job_permalink(); ?>">
-		<div class="position">
-			<h3><?php wpjm_the_job_title(); ?></h3>
+		<div class="image">
+			<?php the_company_logo(); ?>
 		</div>
-		<ul class="meta">
-			<li class="location"><?php the_job_location( false ); ?></li>
-			<li class="company"><?php the_company_name(); ?></li>
-			<?php if ( get_option( 'job_manager_enable_types' ) ) { ?>
-				<?php $types = wpjm_get_the_job_types(); ?>
-				<?php if ( ! empty( $types ) ) : foreach ( $types as $type ) : ?>
-					<li class="job-type <?php echo esc_attr( sanitize_title( $type->slug ) ); ?>"><?php echo esc_html( $type->name ); ?></li>
-				<?php endforeach; endif; ?>
-			<?php } ?>
-		</ul>
+		<div class="content">
+			<div class="position">
+				<h3><?php wpjm_the_job_title(); ?></h3>
+			</div>
+			<ul class="meta">
+				<li class="location"><?php the_job_location( false ); ?></li>
+				<li class="company"><?php the_company_name(); ?></li>
+				<?php if ( get_option( 'job_manager_enable_types' ) ) { ?>
+					<?php $types = wpjm_get_the_job_types(); ?>
+					<?php if ( ! empty( $types ) ) : foreach ( $types as $type ) : ?>
+						<li class="job-type <?php echo esc_attr( sanitize_title( $type->slug ) ); ?>"><?php echo esc_html( $type->name ); ?></li>
+					<?php endforeach; endif; ?>
+				<?php } ?>
+			</ul>
+		</div>
 	</a>
 </li>
