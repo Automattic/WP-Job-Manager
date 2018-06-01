f72c3780e7653160285d83d103a7165722ab5b58
Shantanu <shantanu2846@gmail.com>
2018-06-01 18:30:27 +0530

    Add an empty class to handle the data export

---
+++ includes/class-wp-job-manager-data-exporter.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Defines a class to handle the user data export
+ *
+ * @package wp-job-manager
+ * @since 1.31.1
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( ! class_exists( 'WP_Job_Manager_Data_Exporter' ) ) {
+	/**
+	 * Handles the user data export.
+	 *
+	 * @package
+	 * @since
+	 */
+	class WP_Job_Manager_Data_Exporter {
+
+	}
+}
