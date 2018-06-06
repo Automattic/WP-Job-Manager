cad5999a3d86abffd7687a09f003b193e08cdd4d
Shantanu <shantanu2846@gmail.com>
2018-06-06 15:29:52 +0530

    Add a helper method to the test class to setup the user_meta

--- tests/php/tests/includes/test_class.wp-job-manager-data-exporter.php
+++ tests/php/tests/includes/test_class.wp-job-manager-data-exporter.php
@@ -3,5 +3,30 @@
 require 'includes/class-wp-job-manager-data-exporter';
 
 class WP_Job_Manager_Data_Exporter_Test extends WP_UnitTestCase {
-	
+	/**
+	 * Setup user meta
+	 *
+	 * @param array $args
+	 */
+	private function setupUserMeta( $args ) {
+		$user_id = $this->factory()->user->create(
+			array(
+				'user_login' => 'johndoe',
+				'user_email' => 'johndoe@example.com',
+				'role' => 'subscriber',
+			)
+		);
+
+		if ( isset( $args['_company_logo' ] ) ) {
+			$args['_company_logo'] = $this->factory()->post->create(
+				array(
+					'post_type' => 'attachment'
+				)
+			);
+		}
+
+		foreach ( $args as $key => $value ) {
+			update_user_meta( $user_id, $key, $value );
+		}
+	}
 }
