2ef6cde9a93da0de4ea1b02fc3fc7b281e68650f
Shantanu <shantanu2846@gmail.com>
2018-06-06 15:50:55 +0530

    Add a data provider for the test

--- tests/php/tests/includes/test_class.wp-job-manager-data-exporter.php
+++ tests/php/tests/includes/test_class.wp-job-manager-data-exporter.php
@@ -41,4 +41,81 @@ class WP_Job_Manager_Data_Exporter_Test extends WP_UnitTestCase {
 
 		$this->assertEqual( $expec, $result );
 	}
+
+	public function data_provider(){
+		return [
+			[
+				array(
+					'_company_logo' => 'https://example.com/company/logo',
+					'_company_name' => 'Example',
+					'_company_website' => 'https://example.com/',
+					'_company_tagline' => 'Just another tagline',
+					'_company_twitter' => 'https://twitter.com/example?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',
+					'_company_video' => 'https://example.com/company/video', 
+				),
+				array(
+					'data' => array(
+						'group_id'		 => 'wpjm-user-data',
+						'group_label'	 => __( 'WP Job Manager User Data' ),
+						'data'			 => array(
+							array(
+								'name' => '_company_logo',
+								'value' => 'https://example.com/company/logo',
+							),
+							array(
+								'name' => '_company_name',
+								'value' => 'Example',
+							),
+							array(
+								'name' => '_company_website',
+								'value' => 'https://example.com/',
+							),
+							array(
+								'name' => '_company_tagline',
+								'value' => 'Just another tagline',
+							),
+							array(
+								'name' => '_company_twitter',
+								'value' => 'https://twitter.com/example?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',
+							),
+							array(
+								'name' => '_company_video',
+								'value' => 'https://example.com/company/video',
+							),
+						)
+					),
+					'done' => true,
+				)
+			], // End of first set of parameters
+			[
+				array(
+					'_company_name' => 'Example',
+					'_company_website' => 'https://example.com/',
+					'_company_tagline' => 'Just another tagline',
+
+				),
+				array(
+					'data' => array(
+						'group_id'		 => 'wpjm-user-data',
+						'group_label'	 => __( 'WP Job Manager User Data' ),
+						'data'			 => array(
+							array(
+								'name' => '_company_name',
+								'value' => 'Example',
+							),
+							array(
+								'name' => '_company_website',
+								'value' => 'https://example.com/',
+							),
+							array(
+								'name' => '_company_tagline',
+								'value' => 'Just another tagline',
+							),
+						)
+					),
+					'done' => true,
+				)
+			] // End of second set of parameters
+		];
+	}
 }
