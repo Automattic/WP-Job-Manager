a3f103122293cc2105dcc9d3d0eedb93c8b18572
Drew Butler <hi@dbtlr.com>
2018-06-27 16:41:31 +0100

    Initial import from the add/jobs-shortcode-block branch

---
+++ includes/class-wp-job-manager-blocks.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Handles Job Manager's Gutenberg Blocks.
+ *
+ * @package wp-job-manager
+ * @since 1.32.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * WP_Job_Manager_Blocks
+ */
+class WP_Job_Manager_Blocks {
+	/**
+	 * The static instance of the WP_Job_Manager_Blocks
+	 *
+	 * @var self
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Singleton instance getter
+	 *
+	 * @return self
+	 */
+	public static function get_instance() {
+		if ( ! self::$_instance ) {
+			self::$_instance = new WP_Job_Manager_Blocks();
+		}
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Instance constructor
+	 */
+	private function __construct() {
+		if ( ! function_exists( 'register_block_type' ) ) {
+			return;
+		}
+
+		add_action( 'init', array( $this, 'register_blocks' ) );
+	}
+
+	/**
+	 * Register all Gutenblocks
+	 */
+	public function register_blocks() {
+		// Jobs block.
+		wp_register_script(
+			'wp-job-manager-block-jobs',
+			JOB_MANAGER_PLUGIN_URL . '/assets/build/blocks/jobs.js',
+			array( 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components', 'wp-hooks' ),
+			'1.0.0'
+		);
+		register_block_type( 'wp-job-manager/jobs', array(
+			'editor_script' => 'wp-job-manager-block-jobs',
+		) );
+	}
+}
+
+WP_Job_Manager_Blocks::get_instance();
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+/* global require, module, process, __dirname */
+
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
+const LodashModuleReplacementPlugin = require( 'lodash-webpack-plugin' );
+const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+
+const blockNames = [
+	// Add the name of the block as a directory in the assets/blocks directory.
+];
+
+const webpackConfig = {
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+
+	entry: Object.assign(
+		blockNames.reduce( ( blocks, blockName ) => {
+			const path = `./assets/blocks/${ blockName }/index.jsx`;
+			blocks[ blockName ] = path;
+			return blocks;
+		}, {} )
+	),
+	output: {
+		filename: 'assets/build/blocks/[name].js',
+		path: __dirname,
+	},
+	module: {
+		rules: [
+			{
+				test: /.jsx$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin( [ 'assets/build/blocks' ] ),
+		new LodashModuleReplacementPlugin(),
+		new UglifyJsPlugin(),
+	],
+};
+
+module.exports = webpackConfig;
