0b111841f5db75547edb1ff0469897ed6a953104
Drew Butler <hi@dbtlr.com>
2018-06-27 11:00:50 -0500

    Add Webpack/Eslinting for Gutenberg blocks (#1533)
    * Initial import from the add/jobs-shortcode-block branch
    
    * Fix linting issue and remove job related block include.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
-/* jshint node:true */
-module.exports = function( grunt ){
+/* eslint-disable */
+
+module.exports = function( grunt ) {
 	'use strict';
 
 	grunt.initConfig({
@@ -9,6 +10,7 @@ module.exports = function( grunt ){
 			fonts: 'assets/font',
 			images: 'assets/images',
 			js: 'assets/js',
+			blocks: 'assets/blocks',
 			build: 'tmp/build',
 			svn: 'tmp/release-svn'
 		},
@@ -16,6 +18,12 @@ module.exports = function( grunt ){
 		shell: {
 			buildMixtape: {
 				command: 'node_modules/.bin/mixtape build'
+			},
+			webpack: {
+				command: 'cross-env BABEL_ENV=default NODE_ENV=production webpack'
+			},
+			webpackDev: {
+				command: 'cross-env BABEL_ENV=default webpack --watch'
 			}
 		},
 
@@ -274,7 +282,10 @@ module.exports = function( grunt ){
 
 	grunt.registerTask( 'build-mixtape', [ 'shell:buildMixtape' ] );
 
-	grunt.registerTask( 'build', [ 'gitinfo', 'clean', 'check-mixtape', 'check-mixtape-fatal', 'test', 'copy' ] );
+	grunt.registerTask( 'build-blocks', [ 'shell:webpack' ] );
+	grunt.registerTask( 'build-blocks:dev', [ 'shell:webpackDev' ] );
+
+	grunt.registerTask( 'build', [ 'gitinfo', 'clean', 'check-mixtape', 'check-mixtape-fatal', 'test', 'build-blocks', 'copy' ] );
 
 	grunt.registerTask( 'deploy', [ 'checkbranch:master', 'checkrepo', 'build', 'wp_deploy' ] );
 	grunt.registerTask( 'deploy-unsafe', [ 'build', 'wp_deploy' ] );
