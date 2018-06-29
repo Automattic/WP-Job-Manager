fbde49a5b9ff35ec6697243314784db5877992b9
Jake Oehler Morrison <jake.morrison@automattic.com>
2018-06-29 18:23:35 +0100

    PHPCS: Minor rule ignoring for category walker

--- includes/class-wp-job-manager-category-walker.php
+++ includes/class-wp-job-manager-category-walker.php
@@ -31,6 +31,8 @@ class WP_Job_Manager_Category_Walker extends Walker {
 	);
 
 	/**
+	 * Start the list walker.
+	 *
 	 * @see Walker::start_el()
 	 * @since 2.1.0
 	 *
@@ -54,7 +56,11 @@ class WP_Job_Manager_Category_Walker extends Walker {
 
 		$output .= "\t<option class=\"level-" . intval( $depth ) . '" value="' . esc_attr( $value ) . '"';
 
-		if ( isset( $args['selected'] ) && ( $value == $args['selected'] || ( is_array( $args['selected'] ) && in_array( $value, $args['selected'] ) ) ) ) {
+		if ( isset( $args['selected'] ) && (
+				$value == $args['selected'] // phpcs:ignore WordPress.PHP.StrictComparisons
+				|| ( is_array( $args['selected'] ) && in_array( $value, $args['selected'] ) ) // phpcs:ignore WordPress.PHP.StrictInArray
+			 )
+		) {
 			$output .= ' selected="selected"';
 		}
 
