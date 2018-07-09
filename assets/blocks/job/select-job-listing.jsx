4f4374f177ff0f8eac8cf201e0dc7754bd19b14b
Kuba Birecki <kuba.birecki@crystalplanet.io>
2018-07-09 12:50:13 +0200

    Add SelectJobListing component

---
+++ assets/blocks/job/select-job-listing.jsx
@@ -0,0 +1,27 @@
+/**
+ * External dependencies
+ */
+import { map } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { SelectControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+const SelectJobListing = ( { attributes, jobListings, setAttributes } ) => {
+	const options = map( jobListings, listing => ( {
+		value: listing.id,
+		label: listing.title.rendered,
+	} ) );
+
+	return (
+		<SelectControl
+			label={ __( 'Jobs listings' ) }
+			value={ attributes.jobId }
+			options={ options }
+			onChange={ jobId => setAttributes( { jobId: parseInt( jobId ) } ) } />
+	);
+}
+
+export default SelectJobListing;
