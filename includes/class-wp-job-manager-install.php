fda28f3d215394231200653dcd6607ce786bd480
Jake Oehler Morrison <jake.morrison@automattic.com>
2018-12-13 16:45:40 +0000

    Fix a merge issue

--- includes/class-wp-job-manager-install.php
+++ includes/class-wp-job-manager-install.php
@@ -21,10 +21,9 @@ class WP_Job_Manager_Install {
 		self::init_user_roles();
 		self::default_terms();
 
-		// Fresh installs should be prompted to set up their instance.
 		$is_new_install = false;
 
-		// Redirect to setup screen for new installs.
+		// Fresh installs should be prompted to set up their instance.
 		if ( ! get_option( 'wp_job_manager_version' ) ) {
 			include_once JOB_MANAGER_PLUGIN_DIR . '/includes/admin/class-wp-job-manager-admin-notices.php';
 			WP_Job_Manager_Admin_Notices::add_notice( WP_Job_Manager_Admin_Notices::NOTICE_CORE_SETUP );
