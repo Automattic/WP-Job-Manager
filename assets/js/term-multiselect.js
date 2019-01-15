5097c456398a475cd00f15ad7495dcde56850f06
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-01-15 13:59:17 -0800

    Use global for select2 args and set width to 100%

--- assets/js/term-multiselect.js
+++ assets/js/term-multiselect.js
@@ -1,4 +1,4 @@
-/* global job_manager_select2_multiselect_args */
+/* global job_manager_select2_args */
 jQuery(function(){
-	jQuery( '.job-manager-category-dropdown:visible' ).select2( job_manager_select2_multiselect_args );
+	jQuery( '.job-manager-category-dropdown:visible' ).select2( job_manager_select2_args );
 });
