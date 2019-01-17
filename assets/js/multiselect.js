0dc8fa45d8b7a1f9b4605d8e84b259f6683f396c
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-01-17 10:26:16 -0800

    Fix whitespacing in multiselect js

--- assets/js/multiselect.js
+++ assets/js/multiselect.js
@@ -1,6 +1,6 @@
 /* global job_manager_select2_args */
-jQuery(function($){
+jQuery( function( $ ) {
 	if ( $.isFunction( $.fn.select2 ) && typeof job_manager_select2_args !== 'undefined' ) {
-		$('.job-manager-multiselect:visible').select2(job_manager_select2_args);
+		$( '.job-manager-multiselect:visible' ).select2( job_manager_select2_args );
 	}
-});
+} );
--- assets/js/term-multiselect.js
+++ assets/js/term-multiselect.js
@@ -1,6 +1,6 @@
 /* global job_manager_select2_args */
-jQuery(function($){
+jQuery( function( $ ) {
 	if ( $.isFunction( $.fn.select2 ) && typeof job_manager_select2_args !== 'undefined' ) {
-		$('.job-manager-category-dropdown:visible').select2(job_manager_select2_args);
+		$( '.job-manager-category-dropdown:visible' ).select2( job_manager_select2_args );
 	}
-});
+} );
