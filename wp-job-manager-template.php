49c4927032abefa7a63c7f74570b965886595f09
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-04-24 23:21:20 +0200

    Run raw post content through wp_kses_post

--- wp-job-manager-template.php
+++ wp-job-manager-template.php
@@ -548,13 +548,13 @@ function wpjm_get_the_job_description( $post = null ) {
 		return null;
 	}
 
-	$description = apply_filters( 'the_job_description', $post->post_content );
+	$description = apply_filters( 'the_job_description', wp_kses_post( $post->post_content ) );
 
 	/**
 	 * Filter for the job description.
 	 *
 	 * @since 1.28.0
-	 * @param string      $title Title to be filtered.
+	 * @param string      $job_description Job description to be filtered.
 	 * @param int|WP_Post $post
 	 */
 	return apply_filters( 'wpjm_the_job_description', $description, $post );
