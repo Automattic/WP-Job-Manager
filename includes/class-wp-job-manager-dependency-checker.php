bfb6fa0a7583c86aefa1cfc7e831f8c68e765089
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-05-07 13:09:33 +0100

    Show link to update WordPress only when user has permission

--- includes/class-wp-job-manager-dependency-checker.php
+++ includes/class-wp-job-manager-dependency-checker.php
@@ -53,9 +53,9 @@ class WP_Job_Manager_Dependency_Checker {
 	 */
 	public static function add_php_notice() {
 		$screen        = get_current_screen();
-		$valid_screens = array( 'dashboard', 'plugins', 'plugins-network', 'edit-job_listing', 'job_listing_page_job-manager-settings' );
+		$valid_screens = self::get_critical_screen_ids();
 
-		if ( ! current_user_can( 'activate_plugins' ) || ! in_array( $screen->id, $valid_screens, true ) ) {
+		if ( null === $screen || ! current_user_can( 'activate_plugins' ) || ! in_array( $screen->id, $valid_screens, true ) ) {
 			return;
 		}
 
@@ -95,16 +95,21 @@ class WP_Job_Manager_Dependency_Checker {
 	 * @access private
 	 */
 	public static function add_wp_notice() {
-		// We only want to show the notices on the plugins page and WPJM admin pages.
 		$screen        = get_current_screen();
-		$valid_screens = array( 'dashboard', 'plugins', 'plugins-network', 'edit-job_listing', 'job_listing_page_job-manager-settings' );
+		$valid_screens = self::get_critical_screen_ids();
+
 		if ( null === $screen || ! in_array( $screen->id, $valid_screens, true ) ) {
 			return;
 		}
 
+		$update_action_link = '';
+		if ( current_user_can( 'update_core' ) ) {
+			// translators: %s is the URL for the page where users can go to update WordPress.
+			$update_action_link = ' ' . sprintf( 'Please <a href="%s">update WordPress</a> to avoid issues.', esc_url( self_admin_url( 'update-core.php' ) ) );
+		}
+
 		echo '<div class="error">';
-		// translators: %s is the URL for the page where users can go to update WordPress.
-		echo '<p>' . wp_kses_post( sprintf( __( '<strong>WP Job Manager</strong> requires a more recent version of WordPress. <a href="%s">Please update WordPress</a> to avoid issues.', 'wp-job-manager' ), esc_url( self_admin_url( 'update-core.php' ) ) ) ) . '</p>';
+		echo '<p>' . wp_kses_post( __( '<strong>WP Job Manager</strong> requires a more recent version of WordPress.', 'wp-job-manager' ) . $update_action_link ) . '</p>';
 		echo '</div>';
 	}
 
@@ -117,8 +122,20 @@ class WP_Job_Manager_Dependency_Checker {
 	 * @return array
 	 */
 	public static function wp_version_plugin_action_notice( $actions ) {
-		// translators: Placeholder (%s) is the URL where users can go to update WordPress.
-		$actions[] = wp_kses_post( sprintf( __( '<a href="%s" style="color: red">WordPress Update Required</a>', 'wp-job-manager' ), esc_url( self_admin_url( 'update-core.php' ) ) ) );
+		if ( ! current_user_can( 'update_core' ) ) {
+			$actions[] = '<strong style="color: red">' . esc_html__( 'WordPress Update Required', 'wp-job-manager' ) . '</strong>';
+		} else {
+			$actions[] = '<a href="' . esc_url( self_admin_url( 'update-core.php' ) ) . '" style="color: red">' . esc_html__( 'WordPress Update Required', 'wp-job-manager' ) . '</a>';
+		}
 		return $actions;
+	}
+
+	/**
+	 * Returns the screen IDs where dependency notices should be displayed.
+	 *
+	 * @return array
+	 */
+	private static function get_critical_screen_ids() {
+		return array( 'dashboard', 'plugins', 'plugins-network', 'edit-job_listing', 'job_listing_page_job-manager-settings' );
 	}
 }
