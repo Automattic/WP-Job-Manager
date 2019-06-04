82fbd141a9a7cbef9243b21460a068f4abdc645d
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-06-04 11:35:25 +0100

    Version bump to 1.33.0

--- wp-job-manager.php
+++ wp-job-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Job Manager
  * Plugin URI: https://wpjobmanager.com/
  * Description: Manage job listings from the WordPress admin panel, and allow users to post jobs directly to your site.
- * Version: 1.33.0-beta.2
+ * Version: 1.33.0
  * Author: Automattic
  * Author URI: https://wpjobmanager.com/
  * Requires at least: 4.7.0
