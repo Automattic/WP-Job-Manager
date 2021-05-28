7cd1eb6903f96edc012ab31673995ee916aa65b6
Renatho De Carli Rosa <renatho@automattic.com>
2021-05-28 18:23:20 -0300

    Update field to accept links in the label

--- templates/form-fields/full-line-checkbox-field.php
+++ templates/form-fields/full-line-checkbox-field.php
@@ -14,11 +14,18 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$allowed_label_html = [
+	'a' => [
+		'href'   => [],
+		'target' => [],
+	],
+];
 ?>
 <fieldset class="fieldset-<?php echo esc_attr( $key ); ?> ">
 	<div class="field full-line-checkbox-field <?php echo $field['required'] ? 'required-field' : ''; ?>">
 		<input type="checkbox" class="input-checkbox" name="<?php echo esc_attr( isset( $field['name'] ) ? $field['name'] : $key ); ?>" id="<?php echo esc_attr( $key ); ?>" <?php checked( ! empty( $field['value'] ), true ); ?> value="1" <?php if ( ! empty( $field['required'] ) ) echo 'required'; ?> />
-		<label for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field['label'] ); ?></label>
+		<label for="<?php echo esc_attr( $key ); ?>"><?php echo wp_kses( $field['label'], $allowed_label_html ); ?></label>
 		<?php if ( ! empty( $field['description'] ) ) : ?><small class="description"><?php echo wp_kses_post( $field['description'] ); ?></small><?php endif; ?>
 	</div>
 </fieldset>
