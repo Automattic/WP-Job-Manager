543a5b2ccc7257db35a41e51deee5173e552df21
Gnodesign <ginoaliaj@hotmail.com>
2022-06-20 20:25:00 +0200

    phpcs issue

--- wp-job-manager-functions.php
+++ wp-job-manager-functions.php
@@ -1599,6 +1599,7 @@ function job_manager_count_user_job_listings( $user_id = 0 ) {
 		$user_id = get_current_user_id();
 	}
 
+	// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
 	return $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(ID) FROM {$wpdb->posts} WHERE post_author = %d AND post_type = 'job_listing' AND post_status IN ( 'publish', 'pending', 'expired', 'hidden' );", $user_id ) );
 }
 
