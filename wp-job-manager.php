0274f5788e83a0f30a4f39d6b6c1346a82433245
Alex Sanford <alex.sanford1@gmail.com>
2022-07-18 14:12:25 -0300

    Move to production version number

--- wp-job-manager.php
+++ wp-job-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Job Manager
  * Plugin URI: https://wpjobmanager.com/
  * Description: Manage job listings from the WordPress admin panel, and allow users to post jobs directly to your site.
- * Version: 1.37.0.beta1
+ * Version: 1.37.0
  * Author: Automattic
  * Author URI: https://wpjobmanager.com/
  * Requires at least: 5.7
