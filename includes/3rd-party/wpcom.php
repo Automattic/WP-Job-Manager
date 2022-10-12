5fc12ad0bb54b7a92bf49d4d27d77587271fe7de
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2022-10-12 18:48:21 +0300

    Add support for WP.com Marketplace Addon purchase flow
    When an addon is purchase through the WP.com Marketplace, the new integration would automatically configure the addon with the licensing information.
    
    This is done by using a WPCOM specific filter that's executed after the provision_license event is triggered. This filter contains the licensing information needed by the plugin to connect to WPJobManager.com back-end API.

---
+++ includes/3rd-party/wpcom.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Configure license configuration for WP Job Manager when purchased from WP.com Marketplace.
+ *
+ * @param $result
+ * @param $payload
+ * @param string $event_type
+ *
+ * @return bool
+ */
+function dotcom_marketplace_configure_license_for_wp_job_manager_addon( $result, $payload, string $event_type ) {
+	if ( 'provision_license' !== $event_type ) {
+		return $result;
+	}
+
+	$helper = WP_Job_Manager_Helper::instance();
+	$helper->activate_licence( $payload['wpjm_product_slug'], $payload['license_code'], $payload['email_address'] );
+
+	$messages = $helper->get_messages( $payload['wpjm_product_slug'] );
+
+	$errors = array_filter( $messages, function ( $message ) {
+		return 'error' === $message['type'];
+	} );
+
+	if ( ! empty( $errors ) ) {
+		return new \WP_Error( 'error', 'An error has occurred while installing ' . $payload['wpjm_product_slug'], $errors );
+	}
+
+	return $result;
+}
+
+const WPJM_WPCOM_PRODUCTS = [
+	'wp-job-manager-applications',
+	'wp-job-manager-resumes',
+	'wp-job-manager-simple-paid-listings',
+	'wp-job-manager-wc-paid-listings',
+	'wp-job-manager-tags',
+	'wp-job-manager-bookmarks',
+	'wp-job-manager-alerts',
+	'wp-job-manager-application-deadline',
+	'wp-job-manager-embeddable-job-widget',
+];
+
+foreach ( WPJM_WPCOM_PRODUCTS as $wpjm_wpcom_product ) {
+	add_filter( 'wpcom_marketplace_webhook_response_' . $wpjm_wpcom_product, 'dotcom_marketplace_configure_license_for_wp_job_manager_addon', 10, 3 );
+}
