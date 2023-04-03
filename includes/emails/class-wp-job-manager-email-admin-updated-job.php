6e6bc581ed0f4972abb1f6eb7d4ab5881049302f
Mikey Arce <mikeyarce@gmail.com>
2023-04-03 14:50:42 -0700

    Avoid negation

--- includes/emails/class-wp-job-manager-email-admin-updated-job.php
+++ includes/emails/class-wp-job-manager-email-admin-updated-job.php
@@ -79,7 +79,7 @@ class WP_Job_Manager_Email_Admin_Updated_Job extends WP_Job_Manager_Email_Templa
 	 */
 	public function get_to() {
 		$option = get_option( 'job_manager_email_admin_updated_job', false );
-		return ! $option['email_to'] ? get_option( 'admin_email', false ) : $option['email_to'];
+		return $option['email_to'] ? $option['email_to'] : get_option( 'admin_email', false );
 	}
 
 	/**
