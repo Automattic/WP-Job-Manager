5e73ddc7d41042ac7195bad9297f806138e07519
Jake Oehler Morrison <s+github@jom.im>
2023-04-22 00:26:42 +0100

    Remove add-ons screen messages

--- includes/admin/views/html-admin-page-addons.php
+++ includes/admin/views/html-admin-page-addons.php
@@ -10,27 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 echo '<h1 class="screen-reader-text">' . esc_html__( 'WP Job Manager Add-ons', 'wp-job-manager' ) . '</h1>';
-if ( ! empty( $messages ) ) {
-	foreach ( $messages as $message ) {
-		if ( empty( $message->message ) ) {
-			continue;
-		}
-		$message_type = 'info';
-		if ( isset( $message->type )
-		&& in_array( $message->type, [ 'info', 'success', 'warning', 'error' ], true ) ) {
-			$message_type = $message->type;
-		}
-		$action_label  = isset( $message->action_label ) ? esc_attr( $message->action_label ) : __( 'More Information &rarr;', 'wp-job-manager' );
-		$action_url    = isset( $message->action_url ) ? esc_url( $message->action_url, [ 'http', 'https' ] ) : false;
-		$action_target = isset( $message->action_target ) && 'self' === $message->action_target ? '_self' : '_blank';
-		$action_str    = '';
-		if ( $action_url ) {
-			$action_str = ' <a href="' . esc_url( $action_url ) . '" target="' . esc_attr( $action_target ) . '" class="button">' . esc_html( $action_label ) . '</a>';
-		}
-
-		echo '<div class="notice notice-' . esc_attr( $message_type ) . ' below-h2"><p><strong>' . esc_html( $message->message ) . '</strong>' . wp_kses_post( $action_str ) . '</p></div>';
-	}
-}
 if ( ! empty( $categories ) ) {
 	// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Input is used safely.
 	$current_category = isset( $_GET['category'] ) ? sanitize_text_field( wp_unslash( $_GET['category'] ) ) : '_all';
