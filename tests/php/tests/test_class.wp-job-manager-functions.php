b0cf31c1ad3398caf6e32e263d81412969937b22
onubrooks <abahonuh@gmail.com>
2023-06-11 23:00:09 +0100

    add test for job_manager_renew_job_listing

--- tests/php/tests/test_class.wp-job-manager-functions.php
+++ tests/php/tests/test_class.wp-job-manager-functions.php
@@ -966,4 +966,18 @@ class WP_Test_WP_Job_Manager_Functions extends WPJM_BaseTest {
 		update_option( 'job_manager_renewal_days', 0 );
 		$this->assertFalse( job_manager_job_can_be_renewed( $job_listing ) );
 	}
+
+	/**
+	 * @since $$next-version$$
+	 * @covers ::job_manager_renew_job_listing
+	 */
+	public function test_job_manager_renew_job_listing() {
+		$job_listing_id = $this->factory->job_listing->create();
+		$job_listing    = get_post( $job_listing_id );
+		update_option( 'job_manager_renewal_days', 2 );
+		update_post_meta( $job_listing_id, '_job_expires', wp_date('Y-m-d', strtotime("+1 day") ) );
+		$this->assertTrue( job_manager_job_can_be_renewed( $job_listing ) );
+		job_manager_renew_job_listing( get_post( $job_listing_id ) );
+		$this->assertFalse( job_manager_job_can_be_renewed( $job_listing ) );
+	}
 }
