fcece45506557d01562bf8da00d1eb47b070998b
Mikey Arce <mikeyarce@gmail.com>
2023-06-21 15:37:46 -0700

    Fix lint issues

--- includes/class-wp-job-manager-promoted-jobs-api.php
+++ includes/class-wp-job-manager-promoted-jobs-api.php
@@ -60,11 +60,11 @@ class WP_Job_Manager_Promoted_Jobs_API extends WP_REST_Controller {
 	 */
 	public function get_items( $request ) {
 		$args = [
-			'post_type'   => 'job_listing',
-			'post_status' => 'publish',
-			'no_found_rows' => true,
+			'post_type'           => 'job_listing',
+			'post_status'         => 'publish',
+			'no_found_rows'       => true,
 			'ignore_sticky_posts' => true,
-			'meta_query'  => [
+			'meta_query'          => [
 				[
 					'key'     => '_promoted',
 					'value'   => '1',
@@ -74,8 +74,8 @@ class WP_Job_Manager_Promoted_Jobs_API extends WP_REST_Controller {
 		];
 
 		$job_listings = new WP_Query( $args );
-		$items = $job_listings->posts;
-		$data = [];
+		$items        = $job_listings->posts;
+		$data         = [];
 
 		if ( empty( $items ) ) {
 			return rest_ensure_response( $data );
@@ -83,7 +83,7 @@ class WP_Job_Manager_Promoted_Jobs_API extends WP_REST_Controller {
 
 		foreach ( $items as $item ) {
 			$itemdata = $this->prepare_item_for_response( $item, $request );
-			$data[] = $this->prepare_response_for_collection( $itemdata );
+			$data[]   = $this->prepare_response_for_collection( $itemdata );
 		}
 
 		return new WP_REST_Response( $data, 200 );
@@ -105,15 +105,15 @@ class WP_Job_Manager_Promoted_Jobs_API extends WP_REST_Controller {
 		}
 
 		$data = [
-			'id'              => $item->ID,
-			'title'           => $item->post_title,
-			'description'     => $item->post_content,
-			'permalink'       => get_permalink( $item ),
-			'location'        => get_post_meta( $item->ID, '_job_location', true ),
-			'company_name'    => get_post_meta( $item->ID, '_company_name', true ),
-			'is_remote'       => (bool) get_post_meta( $item->ID, '_remote_position', true ),
-			'job_type'        => $terms_array,
-			'salary'          => [
+			'id'           => $item->ID,
+			'title'        => $item->post_title,
+			'description'  => $item->post_content,
+			'permalink'    => get_permalink( $item ),
+			'location'     => get_post_meta( $item->ID, '_job_location', true ),
+			'company_name' => get_post_meta( $item->ID, '_company_name', true ),
+			'is_remote'    => (bool) get_post_meta( $item->ID, '_remote_position', true ),
+			'job_type'     => $terms_array,
+			'salary'       => [
 				'salary_amount'   => get_post_meta( $item->ID, '_job_salary', true ),
 				'salary_currency' => get_post_meta( $item->ID, '_job_salary_currency', true ),
 				'salary_unit'     => get_post_meta( $item->ID, '_job_salary_unit', true ),
