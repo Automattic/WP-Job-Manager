a50df3f02621c873186228a6722adae9b21ca208
Renatho De Carli Rosa <renatho@gmail.com>
2023-07-25 14:43:21 -0300

    Display promote dialog only after the save is complete

--- assets/js/admin/job-editor.js
+++ assets/js/admin/job-editor.js
@@ -11,6 +11,7 @@ import domReady from '@wordpress/dom-ready';
 import editorLifecycle from './editor-lifecycle';
 import { postOpenPromoteModal } from './promote-job-modals';
 
+// Open promote dialog when it's publishing a job.
 domReady( () => {
 	const coreEditorSelector = select( editorStore );
 	const promoteDialog = document.querySelector( '#promote-dialog' );
@@ -19,13 +20,21 @@ domReady( () => {
 		return;
 	}
 
+	let jobWasPublished = false;
+
 	editorLifecycle( {
 		onSaveStart: () => {
-			// Check if status is being changed to publish.
-			if ( 'publish' === coreEditorSelector.getEditedPostAttribute( 'status' ) && 'publish' !== coreEditorSelector.getCurrentPostAttribute( 'status' ) ) {
+			// Mark if status is being changed to publish.
+			jobWasPublished =
+				'publish' === coreEditorSelector.getEditedPostAttribute( 'status' ) &&
+				'publish' !== coreEditorSelector.getCurrentPostAttribute( 'status' );
+		},
+		onSave: () => {
+			// Open dialog when job was published.
+			if ( jobWasPublished ) {
 				promoteDialog.showModal();
 				postOpenPromoteModal( promoteDialog, window.wpjm.promoteUrl );
 			}
-		}
+		},
 	} );
 } );
