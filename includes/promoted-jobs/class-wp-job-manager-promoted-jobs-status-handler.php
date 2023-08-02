93073e35ed11b82f7fc8973030581c7fc0b6bac0
Fernando Jorge Mota <contato@fjorgemota.com>
2023-08-01 22:14:08 -0300

    Add class WP_Job_Manager_Promoted_Jobs_Status_Handler to handling status update of promoted jobs

---
+++ includes/promoted-jobs/class-wp-job-manager-promoted-jobs-status-handler.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * File containing the class WP_Job_Manager_Promoted_Jobs_Status_Handler.
+ *
+ * @package wp-job-manager
+ */
+
+/**
+ * Handles functionality related to the Promoted Jobs Status Update.
+ *
+ * @since $$next-version$$
+ */
+class WP_Job_Manager_Promoted_Jobs_Status_Handler {
+
+	/**
+	 * The name of the cron hook for updating the promoted job status.
+	 */
+	private const CRON_HOOK = 'wpjm_promoted_jobs_status_update';
+
+	/**
+	 * The name of the option that stores the last time the cron job was executed.
+	 */
+	private const OPTION_KEY = self::CRON_HOOK . '_last_execution';
+
+	/**
+	 * The frequency at which the cron job should be executed.
+	 */
+	private const FREQUENCY_UPDATE = HOUR_IN_SECONDS;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {}
+
+	/**
+	 * Initialize the status handler, sets up the cron job and hooks for fetching updates.
+	 *
+	 * @since $$next-version$$
+	 */
+	public function init() {
+		if ( ! wp_next_scheduled( self::CRON_HOOK ) ) {
+			wp_schedule_event( time(), 'twicedaily', self::CRON_HOOK );
+		}
+		add_action( self::CRON_HOOK, [ $this, 'fetch_updates' ] );
+	}
+
+	/**
+	 * Fetches updates for promoted jobs from the site feed.
+	 * Updates the promotion status of the jobs accordingly.
+	 */
+	public function fetch_updates() {
+		$last_execution_time = get_option( self::OPTION_KEY, 0 );
+		$current_time        = time();
+
+		if ( $current_time - $last_execution_time < self::FREQUENCY_UPDATE ) {
+			// We block the execution if the last execution was less than 1 hour ago.
+			return;
+		}
+
+		// We always update the last execution time, even if the request fails.
+		update_option( self::OPTION_KEY, $current_time );
+
+		$jobs     = $this->request_site_feed();
+		$statuses = wp_list_pluck( $jobs, 'wpjm_status', 'wpjm_id' );
+		foreach ( $statuses as $job_id => $job_status ) {
+			WP_Job_Manager_Promoted_Jobs::update_promotion( $job_id, '1' === $job_status );
+		}
+	}
+
+	/**
+	 * Requests the site feed for promoted jobs.
+	 * Retrieves and decodes the response, returning the jobs data as an array.
+	 *
+	 * @return array An array of promoted jobs.
+	 */
+	private function request_site_feed() {
+		$url      = $this->get_site_feed_url();
+		$response = wp_remote_get( $url );
+		if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+			return [];
+		}
+		$body = wp_remote_retrieve_body( $response );
+		$json = \json_decode( $body, true );
+		if ( ! array_key_exists( 'jobs', $json ) ) {
+			return [];
+		}
+		return $json['jobs'];
+	}
+
+	/**
+	 * Gets the URL for the site feed of promoted jobs.
+	 *
+	 * @return string The site feed URL.
+	 */
+	private function get_site_feed_url() {
+		return add_query_arg( 'site_url', home_url( '', 'https' ), WP_Job_Manager_Helper_API::get_wpjmcom_url() . '/wp-json/wpjm/v1/promoted-jobs/job_status' );
+	}
+
+}
