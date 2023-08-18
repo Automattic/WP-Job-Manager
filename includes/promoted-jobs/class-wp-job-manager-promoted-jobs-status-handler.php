70e7856d2fc3cd8661979ed89f735600a26faae3
Fernando Jorge Mota <fjorgemota@users.noreply.github.com>
2023-08-18 01:17:17 -0300

    Always update LAST_CHECK option when initializing defaults
    In case we decide to update WPJMCOM logic to be more strict and somehow the cron runs before the data there is updated

--- includes/promoted-jobs/class-wp-job-manager-promoted-jobs-status-handler.php
+++ includes/promoted-jobs/class-wp-job-manager-promoted-jobs-status-handler.php
@@ -164,7 +164,7 @@ class WP_Job_Manager_Promoted_Jobs_Status_Handler {
 	 */
 	public static function initialize_defaults() {
 		$webhook_interval = 5 * MINUTE_IN_SECONDS;
-		add_option( self::LAST_CHECK_OPTION_KEY, time() - $webhook_interval, '', false );
+		update_option( self::LAST_CHECK_OPTION_KEY, time() - $webhook_interval, '', false );
 		add_option( self::CRON_INTERVAL_OPTION_KEY, 12 * HOUR_IN_SECONDS, '', false );
 		add_option( self::WEBHOOK_INTERVAL_OPTION_KEY, $webhook_interval, '', false );
 	}
