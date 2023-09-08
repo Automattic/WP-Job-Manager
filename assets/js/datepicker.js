121efc46a4c1cb12bd4041864acc1ec1dd129965
gkaragia <53191348+gkaragia@users.noreply.github.com>
2023-09-08 17:59:30 +0300

    Subscribe datepicker method to wpJobManagerFieldAdded event

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,26 +1,27 @@
 /* global job_manager_datepicker */
-jQuery(document).ready(function($) {
+jQuery(document).on( 'ready wpJobManagerFieldAdded', function() {
 	var datePickerOptions = {
 		altFormat  : 'yy-mm-dd',
 	};
+
 	if ( typeof job_manager_datepicker !== 'undefined' ) {
 		datePickerOptions.dateFormat = job_manager_datepicker.date_format;
 	}
 
-	$( 'input.job-manager-datepicker, input#_job_expires' ).each( function(){
-		var $hidden_input = $( '<input />', { type: 'hidden', name: $(this).attr( 'name' ) } ).insertAfter( $( this ) );
-		$(this).attr( 'name', $(this).attr( 'name' ) + '-datepicker' );
-		$(this).keyup( function() {
-			if ( '' === $(this).val() ) {
+	jQuery( 'input.job-manager-datepicker, input#_job_expires' ).each( function(){
+		var $hidden_input = jQuery( '<input />', { type: 'hidden', name: jQuery(this).attr( 'name' ) } ).insertAfter( jQuery( this ) );
+		jQuery(this).attr( 'name', jQuery(this).attr( 'name' ) + '-datepicker' );
+		jQuery(this).keyup( function() {
+			if ( '' === jQuery(this).val() ) {
 				$hidden_input.val( '' );
 			}
 		} );
-		$(this).datepicker( $.extend( {}, datePickerOptions, { altField: $hidden_input } ) );
-		if ( $(this).val() ) {
-			var dateParts = $(this).val().split("-");
+		jQuery(this).datepicker( jQuery.extend( {}, datePickerOptions, { altField: $hidden_input } ) );
+		if ( jQuery(this).val() ) {
+			var dateParts = jQuery(this).val().split("-");
 			if ( 3 === dateParts.length ) {
 				var selectedDate = new Date(parseInt(dateParts[0], 10), (parseInt(dateParts[1], 10) - 1), parseInt(dateParts[2], 10));
-				$(this).datepicker('setDate', selectedDate);
+				jQuery(this).datepicker('setDate', selectedDate);
 			}
 		}
 	});
