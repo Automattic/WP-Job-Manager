bbfa0aaada9bb6affb7355725fa6445ac85b8435
gkaragia <53191348+gkaragia@users.noreply.github.com>
2023-09-15 18:48:32 +0300

    Handle event in related element only

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,5 +1,5 @@
 /* global job_manager_datepicker */
-jQuery(document).on( 'ready wpJobManagerFieldAdded', function() {
+jQuery(document).on( 'ready', function() {
 	var datePickerOptions = {
 		altFormat  : 'yy-mm-dd',
 	};
@@ -8,21 +8,30 @@ jQuery(document).on( 'ready wpJobManagerFieldAdded', function() {
 		datePickerOptions.dateFormat = job_manager_datepicker.date_format;
 	}
 
-	jQuery( 'input.job-manager-datepicker, input#_job_expires' ).each( function(){
-		var $hidden_input = jQuery( '<input />', { type: 'hidden', name: jQuery(this).attr( 'name' ) } ).insertAfter( jQuery( this ) );
-		jQuery(this).attr( 'name', jQuery(this).attr( 'name' ) + '-datepicker' );
-		jQuery(this).keyup( function() {
-			if ( '' === jQuery(this).val() ) {
+	var initializeDatepicker = function ( targetInput ) {
+		var $hidden_input = jQuery( '<input />', { type: 'hidden', name: jQuery(targetInput).attr( 'name' ) } ).insertAfter( jQuery( targetInput ) );
+		jQuery(targetInput).attr( 'name', jQuery(targetInput).attr( 'name' ) + '-datepicker' );
+		jQuery(targetInput).keyup( function() {
+			if ( '' === jQuery(targetInput).val() ) {
 				$hidden_input.val( '' );
 			}
 		} );
-		jQuery(this).datepicker( jQuery.extend( {}, datePickerOptions, { altField: $hidden_input } ) );
-		if ( jQuery(this).val() ) {
-			var dateParts = jQuery(this).val().split("-");
+		jQuery(targetInput).datepicker( jQuery.extend( {}, datePickerOptions, { altField: $hidden_input } ) );
+		if ( jQuery(targetInput).val() ) {
+			var dateParts = jQuery(targetInput).val().split('-');
 			if ( 3 === dateParts.length ) {
 				var selectedDate = new Date(parseInt(dateParts[0], 10), (parseInt(dateParts[1], 10) - 1), parseInt(dateParts[2], 10));
-				jQuery(this).datepicker('setDate', selectedDate);
+				jQuery(targetInput).datepicker('setDate', selectedDate);
 			}
 		}
+	};
+
+	jQuery( 'input.job-manager-datepicker, input#_job_expires' ).each( function() {
+		initializeDatepicker( this );
+	} );
+
+
+	jQuery( document ).on( 'wpJobManagerFieldAdded', function ( e ) {
+		initializeDatepicker( e.target );
 	});
 });
