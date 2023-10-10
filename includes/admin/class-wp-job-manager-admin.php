3db3f92ebc1321cb1de4f42a0b255b6fd69436dd
Fernando Jorge Mota <contato@fjorgemota.com>
2023-10-10 12:37:20 -0300

    Rename Extensions sub-menu to Add-ons

--- includes/admin/class-wp-job-manager-admin.php
+++ includes/admin/class-wp-job-manager-admin.php
@@ -173,7 +173,7 @@ class WP_Job_Manager_Admin {
 		add_submenu_page( 'edit.php?post_type=job_listing', __( 'Settings', 'wp-job-manager' ), __( 'Settings', 'wp-job-manager' ), 'manage_options', 'job-manager-settings', [ $this->settings_page, 'output' ] );
 
 		if ( WP_Job_Manager_Helper::instance()->has_licensed_products() || apply_filters( 'job_manager_show_addons_page', true ) ) {
-			add_submenu_page( 'edit.php?post_type=job_listing', __( 'WP Job Manager Add-ons', 'wp-job-manager' ), __( 'Add-ons', 'wp-job-manager' ), 'manage_options', 'job-manager-addons', [ $this, 'addons_page' ] );
+			add_submenu_page( 'edit.php?post_type=job_listing', __( 'WP Job Manager Extensions', 'wp-job-manager' ), __( 'Extensions', 'wp-job-manager' ), 'manage_options', 'job-manager-addons', [ $this, 'addons_page' ] );
 		}
 	}
 
