fda357d5bb04d4447c477c30d7328c24ebeee531
Mikey Arce <mikeyarce@gmail.com>
2023-10-12 10:57:47 -0700

    Switch to use SVG for core bundle icon

--- includes/admin/views/html-admin-page-addons.php
+++ includes/admin/views/html-admin-page-addons.php
@@ -49,13 +49,7 @@ if ( empty( $add_ons ) ) {
 	echo '<ul class="products">';
 	foreach ( $add_ons as $add_on ) {
 		$class = '';
-
-		if ( 'Core Add-on&nbsp;Bundle' === $add_on->title ) {
-			$add_on->image = 'https://wpjobmanager.com/wp-content/uploads/2023/09/core-bundle-icon.gif';
-			$class         = 'wpjm-core-bundle';
-		}
-
-		$url = add_query_arg(
+		$url   = add_query_arg(
 			[
 				'utm_source'   => 'product',
 				'utm_medium'   => 'addonpage',
@@ -68,9 +62,29 @@ if ( empty( $add_ons ) ) {
 		<li class="product">
 
 			<div class="add-on-header">
-				<?php if ( ! empty( $add_on->image ) ) : ?>
-					<img class="<?php echo esc_attr( $class ); ?>" src="<?php echo esc_url( remove_query_arg( [ 'w', 'h', 'crop' ], $add_on->image ) ); ?>" />
+			<?php if ( 'https://wpjobmanager.com/add-ons/bundle/' === $add_on->link ) : ?>
+					<div class="wp-block-group jm-logo__wrapper is-nowrap is-layout-flex">
+						<div class="jm-logo">
+							<svg xmlns="http://www.w3.org/2000/svg" class="jm-logo__head" fill="currentColor" viewBox="0 0 80 80">
+								<path fill-rule="evenodd" d="M40 76c19.882 0 36-16.118 36-36S59.882 4 40 4 4 20.118 4 40s16.118 36 36 36Zm0 4c22.091 0 40-17.909 40-40S62.091 0 40 0 0 17.909 0 40s17.909 40 40 40Z"></path>
+							</svg>
+							<div class="jm-logo__inner">
+								<svg xmlns="http://www.w3.org/2000/svg" class="jm-logo__face" fill="currentColor" viewBox="0 0 80 80">
+									<path d="M28.842 41.536a1.611 1.611 0 0 1 2.216.53 10.466 10.466 0 0 0 8.935 5.006c3.778 0 7.09-2 8.935-5.005a1.611 1.611 0 1 1 2.747 1.685 13.689 13.689 0 0 1-11.682 6.543 13.689 13.689 0 0 1-11.682-6.543 1.611 1.611 0 0 1 .531-2.216Zm-.666-18.096a3.223 3.223 0 1 0-6.446 0 3.223 3.223 0 0 0 6.446 0Zm29.274 0a3.223 3.223 0 1 0-6.446 0 3.223 3.223 0 0 0 6.445 0Z"></path>
+								</svg>
+								<svg xmlns="http://www.w3.org/2000/svg" class="jm-logo__letters" fill="currentColor" viewBox="0 0 80 80">
+									<path d="M33.6 42.888V28h-7.2v14.888c0 .695-.12 1.184-.358 1.466-.24.282-.576.424-1.01.424-.521 0-.934-.185-1.238-.554-.304-.391-.456-1.054-.456-1.987h-7.135c0 2.953.825 5.201 2.476 6.743 1.65 1.542 3.877 2.313 6.679 2.313 2.606 0 4.626-.706 6.059-2.117 1.455-1.434 2.183-3.53 2.183-6.288ZM62.924 51.2V28h-8.829l-4.952 13.846L44.061 28H35.2v23.2h7.232V39.565L45.983 51.2h6.19l3.551-11.635V51.2h7.2Z"></path>
+								</svg>
+							</div>
+						</div>
+					</div>
+				<?php else : ?>
+					<?php if ( ! empty( $add_on->image ) && 'https://wpjobmanager.com/add-ons/bundle/' !== $add_on->link ) : ?>
+						<img class="<?php echo esc_attr( $class ); ?>" src="<?php echo esc_url( remove_query_arg( [ 'w', 'h', 'crop' ], $add_on->image ) ); ?>" />
+					<?php endif; ?>
 				<?php endif; ?>
+
+
 
 				<div class="product-info">
 					<div class="title"><?php echo esc_html( $add_on->title ); ?></div>
