670247782c89614ce3a168f5f12af577eb9f88af
gkaragia <53191348+gkaragia@users.noreply.github.com>
2023-11-17 15:37:14 +0200

    Fix WPJM constant

--- wp-job-manager.php
+++ wp-job-manager.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define constants.
-define( 'JOB_MANAGER_VERSION', '2.0.0' );
+define( 'JOB_MANAGER_VERSION', '2.1.0' );
 define( 'JOB_MANAGER_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'JOB_MANAGER_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
 define( 'JOB_MANAGER_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
