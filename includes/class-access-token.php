d38e3464e2e5da2758264a95be211b6615ab5ee6
gkaragia <53191348+gkaragia@users.noreply.github.com>
2023-12-05 16:57:11 +0200

    Simplify token implementation with wp methods

--- includes/class-access-token.php
+++ includes/class-access-token.php
@@ -17,14 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since $$next-version$$
  */
 class Access_Token {
-
-	/**
-	 * An expiry timestamp.
-	 *
-	 * @var int|null
-	 */
-	private ?int $expiry;
-
 	/**
 	 * Token's metadata. They are hashed together with the token.
 	 *
@@ -35,12 +27,10 @@ class Access_Token {
 	/**
 	 * Constructor.
 	 *
-	 * @param array    $metadata Metadata to be hashed together with the token.
-	 * @param int|null $expiry Expiry timestamp.
+	 * @param array $metadata Metadata to be hashed together with the token.
 	 */
-	public function __construct( array $metadata, int $expiry = null ) {
+	public function __construct( array $metadata ) {
 		$this->metadata = $metadata;
-		$this->expiry   = $expiry;
 	}
 
 	/**
@@ -49,94 +39,43 @@ class Access_Token {
 	 * @return string The token.
 	 */
 	public function create() : string {
-		// Create a random token, rtrim and strtr converts the base64 to a url-friendly format.
-		// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode -- Method used safely.
-		$token = rtrim( strtr( base64_encode( openssl_random_pseudo_bytes( 18 ) ), '+/', '-_' ), '=' );
+		$tick = wp_nonce_tick();
 
+		return $this->generate_token( $tick );
+	}
+
+	/**
+	 * Generates a token for a tick.
+	 *
+	 * @param float $tick
+	 *
+	 * @return false|string
+	 */
+	private function generate_token( float $tick ) {
 		$metadata_json = wp_json_encode( $this->metadata );
-		$token_hash    = password_hash( $metadata_json . $token, PASSWORD_DEFAULT );
 
-		$this->store_token_data( $this->metadata, $token_hash, $this->expiry );
-
-		return $token;
+		return substr( wp_hash( $tick . '|' . $metadata_json, 'nonce' ), -12, 10 );
 	}
 
 	/**
 	 * Verifies that a token is correct.
 	 *
 	 * @param string $token The token to verify.
+	 * @param int    $duration_days Days that the token should be valid.
 	 *
 	 * @return bool True if the token is correct.
 	 */
-	public function verify( string $token ) : bool {
-		$stored_data = $this->load_token_data( $this->metadata );
+	public function verify( string $token, int $duration_days ) : bool {
+		$duration_ticks = $duration_days * 2;
+		$tick           = wp_nonce_tick();
 
-		if ( true === $this->check_token_expiry( $stored_data['expiry'] ) ) {
-			return false;
+		for ( $i = 0; $i < $duration_ticks; $i++ ) {
+			if ( hash_equals( $this->generate_token( $tick - $i ), $token ) ) {
+				return true;
+			}
 		}
 
-		return password_verify( wp_json_encode( $this->metadata ) . $token, $stored_data['token_hash'] );
-	}
-
-	/**
-	 * Whether the token is expired.
-	 *
-	 * @return bool
-	 */
-	public function is_expired() : bool {
-		$stored_data = $this->load_token_data( $this->metadata );
-
-		return $this->check_token_expiry( $stored_data['expiry'] );
-	}
-
-	/**
-	 * Checks if a token is expired.
-	 *
-	 * @param int $expiry The token expiry.
-	 *
-	 * @return bool
-	 */
-	private function check_token_expiry( int $expiry ) : bool {
-		return ! empty( $expiry ) && time() > $expiry;
-	}
-
-	/**
-	 * Load the token data from storage.
-	 *
-	 * @param array $metadata The metadata of the token.
-	 *
-	 * @return array $stored_data {
-	 *
-	 * @type string $token_hash The token hash. Used for verification.
-	 * @type string $metadata The stored token metadata.
-	 * @type int    $expiry The expiry timestamp.
-	 * }
-	 */
-	protected function load_token_data( array $metadata ): array {
-		$token_hash      = get_user_meta( $metadata['user_id'], 'job_manager_alerts_secret_key', true );
-		$stored_metadata = get_user_meta( $metadata['user_id'], 'job_manager_alerts_token_metadata', true );
-		$expiry          = (int) get_user_meta( $metadata['user_id'], 'job_manager_alerts_token_expiry', true );
-
-		return [
-			'token_hash' => $token_hash,
-			'metadata'   => $stored_metadata ?: [],
-			'expiry'     => $expiry,
-		];
-	}
-
-	/**
-	 * Store the token data.
-	 *
-	 * @param array    $metadata   The token metadata.
-	 * @param string   $token_hash The token hash that will be used for the verification.
-	 * @param int|null $expiry The token expiry.
-	 *
-	 * @return void
-	 */
-	protected function store_token_data( array $metadata, string $token_hash, ?int $expiry ): void {
-		update_user_meta( $metadata['user_id'], 'job_manager_alerts_secret_key', $token_hash );
-		update_user_meta( $metadata['user_id'], 'job_manager_alerts_token_metadata', $metadata );
-		update_user_meta( $metadata['user_id'], 'job_manager_alerts_token_expiry', $expiry );
+		return false;
 	}
 }
 
--- tests/php/tests/includes/test_class.access-token.php
+++ tests/php/tests/includes/test_class.access-token.php
@@ -2,6 +2,7 @@
 
 namespace WP_Job_Manager;
 
+use ReflectionObject;
 use WP_UnitTestCase;
 
 class WP_Test_Access_Token extends WP_UnitTestCase {
@@ -10,24 +11,37 @@ class WP_Test_Access_Token extends WP_UnitTestCase {
 		$access_token = new Access_Token( [ 'user_id' => 10 ] );
 		$token = $access_token->create();
 
-		self::assertTrue( ( new Access_Token( [ 'user_id' => 10] ))->verify( $token ) );
-		self::assertFalse( ( new Access_Token( [ 'user_id' => 10] ))->verify( 'aRandomString' ) );
+		self::assertTrue( ( new Access_Token( [ 'user_id' => 10] ))->verify( $token, 1 ) );
+		self::assertFalse( ( new Access_Token( [ 'user_id' => 10] ))->verify( 'aRandomString', 1 ) );
 	}
 
 	public function test_expired_token_not_verified() {
-		$access_token = new Access_Token( [ 'user_id' => 10 ], time() - 1000 );
+		$access_token = new Access_Token( [ 'user_id' => 10 ] );
 		$token = $access_token->create();
 
-		self::assertFalse( ( new Access_Token( [ 'user_id' => 10] ))->verify( $token ) );
-		self::assertTrue( ( new Access_Token( [ 'user_id' => 10] ))->is_expired() );
+		$generate_token = ( new ReflectionObject( $access_token ) )->getMethod( 'generate_token' );
+		$generate_token->setAccessible( true );
+		$tick = wp_nonce_tick();
+
+		$token_12_hours = $generate_token->invoke( $access_token, $tick - 1 );
+		$token_24_hours = $generate_token->invoke( $access_token, $tick - 2 );
+		$token_36_hours = $generate_token->invoke( $access_token, $tick - 3 );
+		$token_48_hours = $generate_token->invoke( $access_token, $tick - 4 );
+		$token_60_hours = $generate_token->invoke( $access_token, $tick - 5 );
+
+		self::assertTrue( ( new Access_Token( [ 'user_id' => 10] ))->verify( $token, 2 ) );
+		self::assertTrue( ( new Access_Token( [ 'user_id' => 10] ))->verify( $token_12_hours, 2 ) );
+		self::assertTrue( ( new Access_Token( [ 'user_id' => 10] ))->verify( $token_24_hours, 2 ) );
+		self::assertTrue( ( new Access_Token( [ 'user_id' => 10] ))->verify( $token_36_hours, 2 ) );
+		self::assertFalse( ( new Access_Token( [ 'user_id' => 10] ))->verify( $token_48_hours, 2 ) );
+		self::assertFalse( ( new Access_Token( [ 'user_id' => 10] ))->verify( $token_60_hours, 2 ) );
 	}
 
 	public function test_incorrect_payload_not_verified() {
 		$access_token = new Access_Token( [ 'user_id' => 10 ], time() + 1000 );
 		$token = $access_token->create();
 
-		self::assertFalse( ( new Access_Token( [ 'user_id' => 10 ] ) )->is_expired() );
-		self::assertTrue( ( new Access_Token( [ 'user_id' => 10 ] ))->verify( $token ) );
-		self::assertFalse( ( new Access_Token( [ 'user_id' => 110 ] ) )->verify( $token ) );
+		self::assertTrue( ( new Access_Token( [ 'user_id' => 10 ] ))->verify( $token, 1 ) );
+		self::assertFalse( ( new Access_Token( [ 'user_id' => 110 ] ) )->verify( $token, 1 ) );
 	}
 }
