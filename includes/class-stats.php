d95908fd4a2abdcba8f49b39d06ff3ff941df3eb
Fernando Jorge Mota <fjorgemota@users.noreply.github.com>
2024-01-30 13:23:29 -0300

    Add Stats Schema (#2719)
    Co-authored-by: Panos Kountanis <panosktn@gmail.com>

--- includes/class-stats.php
+++ includes/class-stats.php
@@ -28,7 +28,46 @@ class Stats {
 	 * Do initialization of all the things needed for stats.
 	 */
 	public function init() {
-		// Do init stuff.
+		$this->initialize_wpdb();
+	}
+
+	/**
+	 * Initialize the alias for the stats table on the wpdb object.
+	 *
+	 * @return void
+	 */
+	private function initialize_wpdb() {
+		global $wpdb;
+		if ( isset( $wpdb->wpjm_stats ) ) {
+			return;
+		}
+		$wpdb->wpjm_stats = $wpdb->prefix . 'wpjm_stats';
+		$wpdb->tables[]   = 'wpjm_stats';
+	}
+
+	/**
+	 * Migrate the stats table to the latest version.
+	 *
+	 * @return void
+	 */
+	public function migrate() {
+		global $wpdb;
+		$collate = $wpdb->get_charset_collate();
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		\dbDelta(
+			[
+				"CREATE TABLE {$wpdb->wpjm_stats} (
+				`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+				`date` date NOT NULL,
+				`post_id` bigint(20) DEFAULT NULL,
+				`name` varchar(255) NOT NULL,
+				`group` varchar(255) DEFAULT '',
+				`count` bigint(20) unsigned not null default 1,
+				PRIMARY KEY (`id`),
+				INDEX `idx_wpjm_stats_name_date_group`  (`name`, `date`, `group`)
+			) {$collate}",
+			]
+		);
 	}
 
 	/**
