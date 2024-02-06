ce25e6c602f5489a4bb20cfa530cc2b116d98267
Peter Kiss <peter@yscik.com>
2024-02-06 11:35:19 +0100

    Update syntax

--- assets/js/wpjm-stats.js
+++ assets/js/wpjm-stats.js
@@ -4,37 +4,36 @@ import domReady from '@wordpress/dom-ready';
 
 ( function () {
 	function updateDailyUnique( key ) {
-		var date = new Date();
-		var expiresAtTimestamp = date.getTime() + 24 * 60 * 60 * 1000;
+		const date = new Date();
+		const expiresAtTimestamp = date.getTime() + 24 * 60 * 60 * 1000;
 		window.localStorage[key] = expiresAtTimestamp;
 	}
 
 	function getDailyUnique( name ) {
 		if ( window.localStorage[name] ) {
-			var date = new Date();
-			var now = date.getTime();
-			var expiration = parseInt( window.localStorage[name], 10 );
+			const date = new Date();
+			const now  = date.getTime();
+			const expiration = parseInt( window.localStorage[ name ], 10 );
 			return expiration >= now;
 		}
 		return false;
 	}
 
 	domReady( function () {
-		var statsToRecord = [];
-		var jobStatsSettings = window.job_manager_stats;
-		var ajaxUrl = jobStatsSettings.ajax_url;
-		var ajaxNonce = jobStatsSettings.ajax_nonce;
-		var uniquesToSet = [];
-		var setUniques = function () {
-			uniquesToSet.forEach( function ( uniqueKey ) {
+		const statsToRecord = [];
+		const jobStatsSettings = window.job_manager_stats;
+		const ajaxUrl          = jobStatsSettings.ajax_url;
+		const ajaxNonce      = jobStatsSettings.ajax_nonce;
+		const uniquesToSet = [];
+		const setUniques   = function() {
+			uniquesToSet.forEach( function( uniqueKey ) {
 				updateDailyUnique( uniqueKey );
 			} );
 		};
 
-		jobStatsSettings.stats_to_log.forEach( function ( statToRecord ) {
-			// do something.
-			var statToRecordKey = statToRecord.name;
-			var uniqueKey = statToRecord.unique_key || '';
+		jobStatsSettings.stats_to_log?.forEach( function ( statToRecord ) {
+			const statToRecordKey = statToRecord.name;
+			const uniqueKey       = statToRecord.unique_key || '';
 			if ( uniqueKey.length === 0 ) {
 				statsToRecord.push( statToRecordKey );
 			} else {
@@ -45,11 +44,11 @@ import domReady from '@wordpress/dom-ready';
 			}
 		} );
 
-		var postData = new URLSearchParams( {
+		const postData = new URLSearchParams( {
 			_ajax_nonce: ajaxNonce,
 			post_id: jobStatsSettings.post_id || 0,
 			action: 'job_manager_log_stat',
-			stats: statsToRecord.join( ',' )
+			stats: statsToRecord.join( ',' ),
 		} );
 
 		fetch( ajaxUrl, {
