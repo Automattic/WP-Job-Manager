648dfe8d3d901fc7461a8f84e70e58b937eafb2b
Peter Kiss <peter@yscik.com>
2024-02-28 20:06:57 +0100

    Add cli command to generate test data

--- includes/class-wp-job-manager.php
+++ includes/class-wp-job-manager.php
@@ -10,8 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-use WP_Job_Manager\Stats;
-
 /**
  * Handles core plugin hooks and action setup.
  *
@@ -100,7 +98,9 @@ class WP_Job_Manager {
 		// Init classes.
 		$this->forms      = WP_Job_Manager_Forms::instance();
 		$this->post_types = WP_Job_Manager_Post_Types::instance();
-		$this->stats      = Stats::instance();
+		$this->stats      = WP_Job_Manager\Stats::instance();
+
+		WP_Job_Manager\Dev_Tools::init();
 
 		// Schedule cron jobs.
 		add_action( 'init', [ __CLASS__, 'maybe_schedule_cron_jobs' ] );
