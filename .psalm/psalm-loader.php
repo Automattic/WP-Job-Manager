2d9b06f02c0cad8f273c148d7a5003adc4963e80
Peter Kiss <peter@yscik.com>
2024-03-20 17:37:07 +0100

    Revert "Add wp cli stubs"
    This reverts commit c6cefbf33e6b9a0270b99f1f97461554b497697a.

--- .psalm/psalm-loader.php
+++ .psalm/psalm-loader.php
@@ -69,4 +69,3 @@ if ( ! defined( 'JOB_MANAGER_PLUGIN_DIR' ) ) {
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';
-require_once __DIR__ . '/../vendor/php-stubs/wordpress-cli-stubs/wordpress-cli-stubs.php';
